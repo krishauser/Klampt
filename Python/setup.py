454c92801043cd79f7545b21ae3f60212c1873ea
krishauser <hauser.kris@gmail.com>
2013-12-01 16:12:19 -0500

    moved setup to Python folder for easier installation

--- Python/setup.py
+++ Python/setup.py
@@ -5,16 +5,21 @@ import distutils.util
 import os
 import glob
 
+
+on_win32 = (distutils.util.get_platform()=='win32')
+on_cygwin = distutils.util.get_platform().startswith('cygwin')
+
+
 #these probably do not need to be changed
-klamptDir = '../..'
-krisLibraryDir = 'Library/KrisLibrary'
-odedir = 'Library/ode-0.11.1'
-#odedir = 'Library/ode-0.12'
-gluidir = 'Library/glui-2.36'
+klamptDir = '..'
+krisLibraryDir = klamptDir+'/'+'Library/KrisLibrary'
+odedir = klamptDir+'/'+'Library/ode-0.11.1'
+#odedir = klamptDir+'/'+'Library/ode-0.12'
+gluidir = klamptDir+'/'+'Library/glui-2.36'
 #optional
-assimpDir='Library/assimp--3.0.1270-sdk'
+assimpDir=klamptDir+'/'+'Library/assimp--3.0.1270-sdk'
 haveassimp = os.path.isdir(assimpDir)
-tinyxmlDir = 'Library/tinyxml'
+tinyxmlDir = klamptDir+'/'+'Library/tinyxml'
 #if ODE_DOUBLE is set to true, turn this to true
 odedouble = False
 
@@ -30,8 +35,6 @@ assimpLibDir = assimpDir+'/lib'
 libdirs = [klamptDir+'/lib',krisLibraryDir+'/lib',tinyxmlLibDir,odelibdir,gluilibdir,assimpLibDir,'/usr/lib']
 
 
-on_cygwin = distutils.util.get_platform().startswith('cygwin')
-
 commonfiles = ['pyerr.cpp']
 
 rssourcefiles = commonfiles + ['robotsim.cpp','robotik.cpp','robotsim_wrap.cxx']
@@ -40,58 +43,74 @@ cosourcefiles = commonfiles + ['collide.cpp','collide_wrap.cxx']
 rfsourcefiles = commonfiles + ['rootfind.cpp','pyvectorfield.cpp','rootfind_wrap.cxx']
 
 #compilation defines
-rsdefines = [('TIXML_USE_STL',None)]
+commondefines = []
+if on_win32:
+	commondefines.append(('WIN32',None))
+rsdefines = commondefines+[('TIXML_USE_STL',None)]
 if odedouble:
     rsdefines.append(('dDOUBLE',None))
 else:
     rsdefines.append(('dSINGLE',None))
+if on_win32:
+	libdirs = [klamptDir+'/msvc/Release',klamptDir+'/Library',assimpLibDir+'/assimp_release-dll_win32']
 
 #needed for KrisLibrary to link
 kllibs = ['KrisLibrary','tinyxml','glpk','glui','GL']
 if on_cygwin:
     kllibs[-1] = 'opengl32'
     kllibs.append('glut32')
+#different naming, no TINYXML on windows
+if on_win32:
+	kllibs=['KrisLibrary','tinyxml_STL','glui32','glut32','opengl32','winmm','user32']
 
 #uncomment this if KrisLibrary was built with GSL support
-kllibs += ['gsl']
+#kllibs += ['gsl']
 
 #needed for Klampt to link
 libs = ['Klampt']+kllibs+['ode']
+#different naming on windows
+if on_win32:
+	libs[-1]='ode_double'
 #link to assimp if assimp support is desired
 if haveassimp:
     libs.append('assimp')
 
-setup(name='RobotSim',
-      version='0.2',
-      description='RobotSim extension module',
+
+setup(name='Klampt',
+      version='0.5',
+      description="PyKlamp't",
       author='Kris Hauser',
       author_email='hauserk@indiana.edu',
-      url='https://github.com/krishauser/KrisLibrary',
+      url='https://github.com/krishauser/Klampt',
       ext_modules=[Extension('_robotsim',
-                             [os.path.join('src',f) for f in rssourcefiles],
+                             [os.path.join('klampt/src',f) for f in rssourcefiles],
                              include_dirs=includeDirs,
                              define_macros=rsdefines,
                              library_dirs=libdirs,
                              libraries=libs,
                              language='c++'),
                    Extension('_motionplanning',
-                             [os.path.join('src',f) for f in mpsourcefiles],
+                             [os.path.join('klampt/src',f) for f in mpsourcefiles],
                              include_dirs=includeDirs,
+							 define_macros=commondefines,
                              library_dirs=libdirs,
                              libraries=kllibs,
                              language='c++'),
                    Extension('_collide',
-                             [os.path.join('src',f) for f in cosourcefiles],
+                             [os.path.join('klampt/src',f) for f in cosourcefiles],
                              include_dirs=includeDirs,
+							 define_macros=commondefines,
                              library_dirs=libdirs,
                              libraries=kllibs,
                              language='c++'),
                    Extension('_rootfind',
-                             [os.path.join('src',f) for f in rfsourcefiles],
+                             [os.path.join('klampt/src',f) for f in rfsourcefiles],
                              include_dirs=includeDirs,
+							 define_macros=commondefines,
                              library_dirs=libdirs,
                              libraries=kllibs,
                              language='c++')],
-      py_modules=['robotsim.py','motionplanning.py','collide.py','rootfind.py']
+      py_modules=['klampt.robotsim','klampt.motionplanning','klampt.collide','klampt.rootfind'],
+	  packages=['klampt']
      )
 
