6b29b3829424f4e0e4894b21e68c013faae454db
krishauser <hauser.kris@gmail.com>
2013-12-24 03:26:37 -0500

    New object pose widget

---
+++ View/ObjectPoseWidget.h
@@ -0,0 +1,38 @@
+#ifndef OBJECT_POSE_WIDGET_H
+#define OBJECT_POSE_WIDGET_H
+
+#include <GLdraw/Widget.h>
+#include <GLdraw/TransformWidget.h>
+#include <robotics/IK.h>
+#include "ViewRigidObject.h"
+using namespace GLDraw;
+
+/** @addtogroup View */
+/** @{ */
+
+/** A widget that allows full posing and editing of the robot config including
+ * IK constraints and base motion.
+ */
+class RigidObjectPoseWidget : public WidgetSet
+{
+public:
+  RigidObjectPoseWidget();
+  RigidObjectPoseWidget(RigidObject* object,ViewRigidObject* viewObject);
+  void Set(RigidObject* object,ViewRigidObject* viewObject);
+
+  const RigidTransform& Pose() const { return poser.T; }
+  void SetPose(const RigidTransform& q);
+
+  virtual void DrawGL(Camera::Viewport& viewport);
+  virtual bool BeginDrag(int x,int y,Camera::Viewport& viewport,double& distance);
+  virtual void Drag(int dx,int dy,Camera::Viewport& viewport);
+  virtual void EndDrag();
+  virtual void Keypress(char c);
+
+  RigidObject* rigidObject;
+  TransformWidget poser;
+};
+
+/** @} */
+
+#endif
