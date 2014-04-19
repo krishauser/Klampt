6aa76d8b15a35b77fb9c9f5a9948db77b73a850d
krishauser <hauser.kris@gmail.com>
2014-04-18 22:12:01 -0400

    Fixed ROS -> serial relay

--- Python/control/rosserialrelay.py
+++ Python/control/rosserialrelay.py
@@ -10,3 +10,9 @@ rospy.init_node('klampt_sim')
 c = controller.MultiController()
 c.launch(RosTimeController())
 c.launch(RosRobotController(robotName,linkNames))
+
+#launch the serial client to connect to a given host and relay messages from the socket to/from ROS
+host = 'localhost'
+port = 3456
+s = ControllerClient((host,port),c)
+asyncore.loop()
