3b08d18960d2633777efff516f0a9822fe797122
krishauser <hauser.kris@gmail.com>
2014-05-16 10:53:17 -0400

    Improved / added planning demos

--- Examples/contactplan.cpp
+++ Examples/contactplan.cpp
@@ -1,33 +1,22 @@
 #include "Planning/StanceCSpace.h"
 #include <planning/AnyMotionPlanner.h>
 #include <utils/ioutils.h>
+#include <utils/stringutils.h>
+#include "Modeling/Paths.h"
 #include "Modeling/MultiPath.h"
 #include "IO/XmlWorld.h"
+#include <Timer.h>
 #include <string.h>
 #include <fstream>
 
-///A margin used in StancePlan on the distance between the COM and the edge of the support polygon
+///A margin used in StancePlan on the distance between the COM and the
+///edge of the support polygon
 Real gSupportPolygonMargin = 0;
-
-bool PlanToSpace(CSpace* space,const Config& qstart,CSpace* goalSpace,
-		 MilestonePath& path,
-		 const HaltingCondition& cond,const string& plannerSettings="")
-{
-  MotionPlannerFactory factory;
-  if(!plannerSettings.empty())
-    factory.LoadJSON(plannerSettings);
-  //do more planner setup here if desired, e.g., change planner type,
-  //perturbation size, connection radius, etc
-  MotionPlannerInterface* planner = factory.Create(space,qstart,goalSpace);
-  string res = planner->Plan(path,cond);
-  cout<<"Planner terminated with condition "<<res<<endl;
-  delete planner;
-  return !path.edges.empty();
-}
 
 
 /** @brief Performs path planning in collision-free space for the
- * given robot at the stance sstart and destination stance sgoal.
+ * given robot at the stance s between start configuration qstart and
+ * destination qgoal.
  * Stability is tested at each configuration.
  *
  * The output is given in path.  cond controls the number of iterations/time
@@ -36,53 +25,79 @@ bool PlanToSpace(CSpace* space,const Config& qstart,CSpace* goalSpace,
  * The constraint specifications are given in WorldPlannerSettings. If you
  * have custom requirements, you will need to set them up.
  */
-bool StancePlan(RobotWorld& world,int robot,const Config& qstart,const Stance& sstart,const Stance& sgoal,
-		MilestonePath& path,
-		const HaltingCondition& cond,const string& plannerSettings="")
+bool StancePlan(RobotWorld& world,int robot,Config& qstart,Config& qgoal,const Stance& s,MilestonePath& path,
+		 const HaltingCondition& cond,const string& plannerSettings="")
 {
   WorldPlannerSettings settings;
   settings.InitializeDefault(world);
   //do more constraint setup here (modifying the settings object) if desired,
   //e.g., set collision margins, edge collision checking resolution, etc.
   StanceCSpace cspace(world,robot,&settings); 
-  StanceCSpace transitionCspace(world,robot,&settings); 
-  cspace.SetStance(sstart);
+  cspace.SetStance(s);
   cspace.CalculateSP();
   if(gSupportPolygonMargin != 0)
     cspace.SetSPMargin(gSupportPolygonMargin);
-
-  //need to determine the transition stance conditions
-  vector<Hold> addedHolds,removedHolds;
-  GetDifference(sstart,sgoal,removedHolds);
-  GetDifference(sgoal,sstart,addedHolds);
-  if(!addedHolds.empty() && !removedHolds.empty()) {
-    fprintf(stderr,"StancePlan: A stance change must either remove or add holds, not both\n");
+  //sanity checking
+  if(!cspace.CheckContact(qstart)) {
+    printf("Start configuration does not meet contact constraint, error %g\n",cspace.ContactDistance(qstart));
+    if(!cspace.SolveContact()) {
+      printf("  Solving for contact failed.\n");
+      return false;
+    }
+    else {
+      printf("  IK problem was solved, using new start configuration\n");
+      qstart = cspace.GetRobot()->q;
+    }
+  }
+  if(!cspace.CheckContact(qgoal)) {
+    printf("Goal configuration does not meet contact constraint, error %g\n",cspace.ContactDistance(qgoal));
+    if(!cspace.SolveContact()) {
+      printf("  Solving for contact failed.\n");
+      return false;
+    }
+    else {
+      printf("  IK problem was solved, using new goal configuration\n");
+      qgoal = cspace.GetRobot()->q;
+    }
+  }
+  if(!cspace.IsFeasible(qstart)) {
+    cout<<"Start configuration is infeasible, violated constraints:"<<endl;
+    vector<bool> infeasible;
+    cspace.CheckObstacles(qstart,infeasible);
+    for(size_t i=0;i<infeasible.size();i++)
+      if(infeasible[i]) cout<<"  "<<cspace.ObstacleName(i)<<endl;
     return false;
   }
-  if(addedHolds.empty()) {
-    transitionCspace.SetStance(sgoal);
-    //add the IK constraint of the removed holds but not the contacts
-    for(size_t i=0;i<removedHolds.size();i++) {
-      removedHolds[i].contacts.clear();
-      transitionCspace.SetHold(removedHolds[i]);
-    }
-  }
-  else {
-    transitionCspace.SetStance(sstart);
-    //add the IK constraint of the added holds but not the contacts
-    for(size_t i=0;i<addedHolds.size();i++) {
-      addedHolds[i].contacts.clear();
-      transitionCspace.SetHold(addedHolds[i]);
-    }
+  if(!cspace.IsFeasible(qgoal)) {
+    cout<<"Goal configuration is infeasible, violated constraints:"<<endl;
+    vector<bool> infeasible;
+    cspace.CheckObstacles(qgoal,infeasible);
+    for(size_t i=0;i<infeasible.size();i++)
+      if(infeasible[i]) cout<<"  "<<cspace.ObstacleName(i)<<endl;
+    return false;
   }
 
-  //plan to reach the transition cspace
-  return PlanToSpace(&cspace,qstart,&transitionCspace,path,cond,plannerSettings);
+  MotionPlannerFactory factory;
+  if(!plannerSettings.empty())
+    factory.LoadJSON(plannerSettings);
+  //do more planner setup here if desired, e.g., change planner type,
+  //perturbation size, connection radius, etc
+
+  //make the planner and do the planning
+  MotionPlannerInterface* planner = factory.Create(&cspace,qstart,qgoal);
+  string res = planner->Plan(path,cond);
+  cout<<"Planner terminated with condition "<<res<<endl;
+  printf("  Stats: SolveContact %d, %gs. IsFeasible %d, %gs\n",cspace.numSolveContact,cspace.solveContactTime,cspace.numIsFeasible,cspace.isFeasibleTime);
+  delete planner;
+  return !path.edges.empty();
 }
 
+
 /** @brief Performs path planning in collision-free space for the
- * given robot at the stance sstart and destination stance sgoal.
- * Stability is not tested.
+ * given robot at the stance s between start configuration qstart and
+ * destination qgoal
+ * Stability is *not* tested.  If you wish to maintain stability use
+ * StancePlan
  *
  * The output is given in path.  cond controls the number of iterations/time
  * for planning.
@@ -90,7 +105,7 @@ bool StancePlan(RobotWorld& world,int robot,const Config& qstart,const Stance& s
  * The constraint specifications are given in WorldPlannerSettings. If you
  * have custom requirements, you will need to set them up.
  */
-bool ContactPlan(RobotWorld& world,int robot,const Config& qstart,const Stance& sstart,const Stance& sgoal,MilestonePath& path,
+bool ContactPlan(RobotWorld& world,int robot,Config& qstart,Config& qgoal,const Stance& s,MilestonePath& path,
 		 const HaltingCondition& cond,const string& plannerSettings="")
 {
   WorldPlannerSettings settings;
@@ -98,36 +113,70 @@ bool ContactPlan(RobotWorld& world,int robot,const Config& qstart,const Stance& 
   //do more constraint setup here (modifying the settings object) if desired,
   //e.g., set collision margins, edge collision checking resolution, etc.
   ContactCSpace cspace(world,robot,&settings); 
-  ContactCSpace transitionCspace(world,robot,&settings); 
-  for(Stance::const_iterator i=sstart.begin();i!=sstart.end();i++)
+  for(Stance::const_iterator i=s.begin();i!=s.end();i++)
     cspace.AddContact(i->second.ikConstraint);
-  vector<Hold> addedHolds,removedHolds;
-  GetDifference(sgoal,sstart,addedHolds);
-
-  if(addedHolds.empty()) {
-    //no added holds! no change needed
-    path.edges.resize(1);
-    path.edges[0] = cspace.LocalPlanner(qstart,qstart);
-    return true;
-  }
-  else {
-    for(Stance::const_iterator i=sstart.begin();i!=sstart.end();i++)
-      transitionCspace.AddContact(i->second.ikConstraint);
-    //add the IK constraint of the added holds 
-    for(size_t i=0;i<addedHolds.size();i++) {
-      transitionCspace.AddContact(addedHolds[i].ikConstraint);
+  //sanity checking
+  if(!cspace.CheckContact(qstart)) {
+    printf("Start configuration does not meet contact constraint, error %g\n",cspace.ContactDistance(qstart));
+    if(!cspace.SolveContact()) {
+      printf("  Solving for contact failed.\n");
+      return false;
+    }
+    else {
+      printf("  IK problem was solved, using new start configuration\n");
+      qstart = cspace.GetRobot()->q;
     }
   }
+  if(!cspace.CheckContact(qgoal)) {
+    printf("Goal configuration does not meet contact constraint, error %g\n",cspace.ContactDistance(qgoal));
+    if(!cspace.SolveContact()) {
+      printf("  Solving for contact failed.\n");
+      return false;
+    }
+    else {
+      printf("  IK problem was solved, using new goal configuration\n");
+      qgoal = cspace.GetRobot()->q;
+    }
+  }
+  if(!cspace.IsFeasible(qstart)) {
+    cout<<"Start configuration is infeasible, violated constraints:"<<endl;
+    vector<bool> infeasible;
+    cspace.CheckObstacles(qstart,infeasible);
+    for(size_t i=0;i<infeasible.size();i++)
+      if(infeasible[i]) cout<<"  "<<cspace.ObstacleName(i)<<endl;
+    return false;
+  }
+  if(!cspace.IsFeasible(qgoal)) {
+    cout<<"Goal configuration is infeasible, violated constraints:"<<endl;
+    vector<bool> infeasible;
+    cspace.CheckObstacles(qgoal,infeasible);
+    for(size_t i=0;i<infeasible.size();i++)
+      if(infeasible[i]) cout<<"  "<<cspace.ObstacleName(i)<<endl;
+    return false;
+  }
 
-  //plan to reach the transition cspace
-  return PlanToSpace(&cspace,qstart,&transitionCspace,path,cond);
+  MotionPlannerFactory factory;
+  if(!plannerSettings.empty())
+    factory.LoadJSON(plannerSettings);
+  //do more planner setup here if desired, e.g., change planner type,
+  //perturbation size, connection radius, etc
+
+  //make the planner and do the planning
+  Timer timer;
+  MotionPlannerInterface* planner = factory.Create(&cspace,qstart,qgoal);
+  cout<<"  Create time: "<<timer.ElapsedTime()<<endl;
+  string res = planner->Plan(path,cond);
+  cout<<"Planner terminated with condition "<<res<<endl;
+  printf("  Stats: SolveContact %d, %gs. IsFeasible %d, %gs\n",cspace.numSolveContact,cspace.solveContactTime,cspace.numIsFeasible,cspace.isFeasibleTime);
+  delete planner;
+  return !path.edges.empty();
 }
 
 
 int main(int argc,const char** argv)
 {
-  if(argc <= 2) {
-    printf("USAGE: ContactPlan [options] world_file stance0 stance1 ...\n");
+  if(argc <= 3) {
+    printf("USAGE: ContactPlan [options] world_file configs stance\n");
     printf("OPTIONS:\n");
     printf("-o filename: the output linear path or multipath (default contactplan.xml)\n");
     printf("-p settings: set the planner configuration file\n");
@@ -185,10 +234,15 @@ int main(int argc,const char** argv)
     else break;
   }
   if(i+3 < argc) {
-    printf("USAGE: ContactPlan [options] world_file stance0 stance1 ...\n");
+    printf("USAGE: ContactPlan [options] world_file configs stance\n");
     return 1;
   }
+  if(i+3 > argc) {
+    printf("Warning: extra arguments provided\n");
+  }
   const char* worldfile = argv[i];
+  const char* configsfile = argv[i+1];
+  const char* stancefile = argv[i+2];
 
   //Read in the world file
   XmlWorld xmlWorld;
@@ -202,18 +256,46 @@ int main(int argc,const char** argv)
     return 1;
   }
 
-  vector<Stance> stances;
-  for(int k=i+1;k<argc;k++) {
-    stances.resize(stances.size()+1);
-    ifstream in(argv[k],ios::in);
-    in >> stances.back();
+  vector<Config> configs;
+  {
+    //Read in the configurations specified in configsfile
+    ifstream in(configsfile);
     if(!in) {
-      printf("Error loading stance file %s\n",argv[k]);
+      printf("Error opening configs file %s\n",configsfile);
+      return false;
+    }
+    while(in) {
+      Config temp;
+      in >> temp;
+      if(in) configs.push_back(temp);
+    }
+    if(configs.size() < 2) {
+      printf("Configs file does not contain 2 or more configs\n");
       return 1;
     }
+    in.close();
   }
 
-  //set up the command line, store it into the MultiPath
+  Stance stance;
+  {
+    //read in the stance specified by stancefile
+    ifstream in(stancefile,ios::in);
+    in >> stance;
+    if(!in) {
+      printf("Error loading stance file %s\n",stancefile);
+      return 1;
+    }
+    in.close();
+  }
+
+  //If the stance has no contacts, use ContactPlan.  Otherwise, use StancePlan
+  bool ignoreContactForces = false;
+  if(NumContactPoints(stance)==0) {
+    printf("No contact points in stance, planning without stability constraints\n");
+    ignoreContactForces = true;
+  }
+
+  //set up the command line, store it into the MultiPath settings
   string cmdline;
   cmdline = argv[0];
   for(int i=1;i<argc;i++) {
@@ -226,20 +308,25 @@ int main(int argc,const char** argv)
   //begin planning
   bool feasible = true;
   Config qstart = world.robots[robot].robot->q;
-  for(size_t i=0;i+1<stances.size();i++) {
+  for(size_t i=0;i+1<configs.size();i++) {
     MilestonePath mpath;
-    if(!StancePlan(world,robot,qstart,stances[i],stances[i+1],mpath,termCond,plannerSettings)) {
+    bool res = false;
+    if(ignoreContactForces)
+      res = ContactPlan(world,robot,configs[i],configs[i+1],stance,mpath,termCond,plannerSettings);
+    else
+      res = StancePlan(world,robot,configs[i],configs[i+1],stance,mpath,termCond,plannerSettings);
+    if(!res) {
       printf("Planning from stance %d to %d failed\n",i,i+1);
       path.sections.resize(path.sections.size()+1);
-      path.SetStance(stances[i],path.sections.size()-1);
-      path.sections.back().milestones[0] = qstart;
-      path.sections.back().milestones[1] = qstart;
+      path.SetStance(stance,path.sections.size()-1);
+      path.sections.back().milestones[0] = configs[i];
+      path.sections.back().milestones[1] = configs[i+1];
       break;
     }
     else {
       path.sections.resize(path.sections.size()+1);
       path.sections.back().milestones.resize(mpath.NumMilestones());
-      path.SetStance(stances[i],path.sections.size()-1);
+      path.SetStance(stance,path.sections.size()-1);
       for(int j=0;j<mpath.NumMilestones();j++)
 	path.sections.back().milestones[j] = mpath.GetMilestone(j);
       qstart = path.sections.back().milestones.back();
@@ -249,6 +336,16 @@ int main(int argc,const char** argv)
     printf("Path planning success! Saving to %s\n",outputfile);
   else
     printf("Path planning failure. Saving placeholder path to %s\n",outputfile);
-  path.Save(outputfile);
+  const char* ext = FileExtension(outputfile);
+  if(ext && 0==strcmp(ext,"path")) {
+    printf("Converted to linear path format\n");
+    LinearPath lpath;
+    Convert(path,lpath);
+    ofstream f(outputfile,ios::out);
+    lpath.Save(f);
+    f.close();
+  }
+  else 
+    path.Save(outputfile);
   return 0;
 }
