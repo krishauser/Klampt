7c26f6810381c38ec8d53187c6705e536b48d864
krishauser <hauser.kris@gmail.com>
2014-05-27 00:58:49 -0400

    Typo fixed

--- Main/pack.cpp
+++ Main/pack.cpp
@@ -154,156 +154,6 @@ string AutoTypePack(ResourceLibrary& lib)
   return "";
 }
 
-ConfigsResource* PackConfigs(ResourceLibrary& lib)
-{
-  std::vector<ConfigResource*> configs = lib.GetPtrsByType<ConfigResource>();
-  ConfigsResource* c=new ConfigsResource;
-  for(size_t i=0;i<configs.size();i++)
-    c->configs.push_back(configs[i]->data);
-  return c;
-}
-
-StanceResource* PackStance(ResourceLibrary& lib)
-{
-  std::vector<HoldResource*> holds = lib.GetPtrsByType<HoldResource>();
-  std::vector<IKGoalResource*> ikgoals = lib.GetPtrsByType<IKGoalResource>();
-  StanceResource* s=new StanceResource;
-  for(size_t i=0;i<holds.size();i++)
-    s->stance.insert(holds[i]->data);
-  for(size_t i=0;i<ikgoals.size();i++) {
-    Hold h;
-    h.link = ikgoals[i]->data.link;
-    h.ikConstraint = ikgoals[i]->data;
-    s->stance.insert(h);
-  }
-  return s;
-}
-
-
-HoldResource* PackHold(ResourceLibrary& lib)
-{
-  std::vector<IKGoalResource*> ikgoals = lib.GetPtrsByType<IKGoalResource>();
-  std::vector<FloatArrayResource*> contacts = lib.GetPtrsByType<FloatArrayResource>();
-  if(ikgoals.size() != 1) {
-    fprintf(stderr,"Trying to pack more than 1 ik goal into a hold\n");
-    return NULL;
-  }
-  HoldResource* h=new HoldResource;
-  h->data.link = ikgoals[0]->data.link;
-  h->data.ikConstraint = ikgoals[0]->data;
-  h->data.contacts.resize(contacts.size());
-  for(size_t i=0;i<contacts.size();i++) {
-    if(contacts[i]->data.size() != 7) {
-      fprintf(stderr,"Contact point doesn't have 7 entries x y z nx ny nz kf\n");
-      return NULL;
-    }
-    h->data.contacts[i].x.set(&contacts[i]->data[0]);
-    h->data.contacts[i].n.set(&contacts[i]->data[3]);
-    h->data.contacts[i].kFriction = contacts[i]->data[6];
-  }
-  return h;
-}
-
-
-LinearPathResource* PackLinearPath(ResourceLibrary& lib)
-{
-  std::vector<FloatArrayResource*> times = lib.GetPtrsByType<FloatArrayResource>();
-  std::vector<ConfigResource*> configs = lib.GetPtrsByType<ConfigResource>();
-  std::vector<ConfigsResource*> configss = lib.GetPtrsByType<ConfigsResource>();
-  if(times.size() != 1) {
-    fprintf(stderr,"Trying to pack more than 1 time vector into a linear path\n");
-    return NULL;
-  }
-  if(configs.empty()) {
-    if(configss.size() != 1) {
-      fprintf(stderr,"Trying to pack more than 1 config set into a linear path\n");
-      return NULL;
-    }
-    LinearPathResource* p=new LinearPathResource;
-    p->times = times[0]->data;
-    p->milestones = configss[0]->configs;
-    return p;
-  }
-  else {
-    if(!configss.empty()) {
-      fprintf(stderr,"Cannot combine config's and config sets into a linear path\n");
-      return NULL;
-    }
-    if(times[0]->data.size() != configs.size()) {
-      fprintf(stderr,"Mismatch in time vector vs configs: %d vs %d\n",times[0]->data.size(),configs.size());
-      return NULL;
-    }
-    LinearPathResource* p=new LinearPathResource;
-    p->times = times[0]->data;
-    for(size_t i=0;i<configs.size();i++) {
-      p->milestones.push_back(configs[i]->data);
-    }
-    return p;
-  }
-}
-
-MultiPathResource* PackMultiPath(ResourceLibrary& lib)
-{
-  std::vector<LinearPathResource*> linearPaths = lib.GetPtrsByType<LinearPathResource>();
-  std::vector<ConfigsResource*> configs = lib.GetPtrsByType<ConfigsResource>();
-  std::vector<StanceResource*> stances = lib.GetPtrsByType<StanceResource>();
-  std::vector<MultiPathResource*> multiPaths = lib.GetPtrsByType<MultiPathResource>();
-  if(linearPaths.empty() && configs.empty() && multiPaths.empty()) {
-    fprintf(stderr,"MultiPath must consist of at least one path section\n");
-    return NULL;
-  }
-  int cnt = (linearPaths.empty() ? 0 : 1) + (configs.empty() ? 0 : 1) + (multiPaths.empty() ? 0 : 1);
-  if(cnt != 1) {
-    fprintf(stderr,"MultiPath may not consist of multiple types of path sections\n");
-    return NULL;
-  }
-  MultiPathResource* p = new MultiPathResource;
-  if(!multiPaths.empty()) {
-    //concatenate paths
-    for(size_t i=0;i<multiPaths.size();i++) {
-      //merge settings
-      for(map<string,string>::const_iterator j=multiPaths[i]->path.settings.begin();j!=multiPaths[i]->path.settings.end();j++)
-	p->path.settings[j->first] = j->second;
-      //can't do global holds yet
-      Assert(multiPaths[i]->path.holdSet.empty());
-      Assert(multiPaths[i]->path.holdSetNames.empty());
-      //append sections
-      p->path.sections.insert(p->path.sections.end(),multiPaths[i]->path.sections.begin(),multiPaths[i]->path.sections.end());
-    }
-  }
-  if(!configs.empty()) {
-    if(configs.size() != 1 && configs.size() != stances.size()) {
-      delete p;
-      fprintf(stderr,"Mismatch in number of stances and config sets\n");
-      return NULL;
-    }
-    p->path.sections.resize(configs.size());
-    for(size_t i=0;i<configs.size();i++)
-      p->path.sections[i].milestones = configs[i]->configs;
-    if(!stances.empty()) {
-      for(size_t i=0;i<stances.size();i++)      
-	p->path.SetStance(stances[i]->stance,i);
-    }
-  }
-  if(!linearPaths.empty()) {
-    if(linearPaths.size() != 1 && linearPaths.size() != stances.size()) {
-      delete p;
-      fprintf(stderr,"Mismatch in number of stances and LinearPaths\n");
-      return NULL;
-    }
-    p->path.sections.resize(linearPaths.size());
-    for(size_t i=0;i<linearPaths.size();i++) {
-      p->path.sections[i].times = linearPaths[i]->times;
-      p->path.sections[i].milestones = linearPaths[i]->milestones;
-    }
-    if(!stances.empty()) {
-      for(size_t i=0;i<stances.size();i++)      
-	p->path.SetStance(stances[i]->stance,i);
-    }
-  }
-  return p;
-}
-
 bool Pack(ResourceLibrary& lib,const char* outname,const char* outtype="auto")
 {
   string type = outtype;
@@ -313,37 +163,19 @@ bool Pack(ResourceLibrary& lib,const char* outname,const char* outtype="auto")
     cout<<"Auto-detected type: "<<type<<endl;
   }
   ResourcePtr r;
-  if(type=="Configs") {
-    r = PackConfigs(lib);
-  }
-  else if(type=="Stance") {
-    r = PackStance(lib);
-  }
-  else if(type=="Hold") {
-    r = PackHold(lib);
-  }
-  else if(type=="LinearPath") {
-    r = PackLinearPath(lib);
-  }
-  else if(type=="MultiPath") {
-    r = PackMultiPath(lib);
-  }
-  /*else if(type=="World") {
-    r = PackWorld(lib);
-  }
-  */
-  else if(type=="xml") {
+  if(type=="xml") {
     return lib.SaveXml(outname);
   }
   else {
-    printf("Unable to pack type %s\n",type.c_str());
-    return false;
+    string errorMessage;
+    r = PackResources(lib,type,&errorMessage);
+    if(!r) {
+      printf("Unable to pack type %s\n",type.c_str());
+      printf("  Error message: %s\n",errorMessage.c_str());
+      return false;
+    }
   }
 
-  if(r == NULL) {
-    printf("Error packing type %s\n",type.c_str());
-    return false;
-  }
   r->name = outname;
   r->fileName = lib.DefaultFileName(r);
   cout<<"Saving to "<<r->fileName<<endl;
