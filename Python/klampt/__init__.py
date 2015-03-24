5fead598066238cbe92d57325c5f0d2d1b608858
krishauser <hauser.kris@gmail.com>
2015-03-24 10:31:40 -0400

    Removed collide module from __init__.py

--- Python/klampt/__init__.py
+++ Python/klampt/__init__.py
@@ -3,7 +3,7 @@ from motionplanning import *
 __all__ = ['WorldModel','RobotModel','RobotModelLink','RigidObjectModel','TerrainModel',
            'SimRobotController','SimBody','Simulator',
            'IKObjective','IKSolver','GeneralizedIKObjective','GeneralizedIKSolver',
-           'ik','contact','collide','trajectory',
+           'ik','contact','trajectory',
            'cspace','cspaceutils','robotcspace',
            'so3','se3','vectorops','robotcollide']
 
