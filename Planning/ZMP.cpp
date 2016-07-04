223a7c011704deb44cc9e5294e797f064af3b21b
motion <motion@scovel.egr.duke.edu>
2016-07-04 12:02:44 -0400

    Added zmp calculations file

---
+++ Planning/ZMP.cpp
@@ -0,0 +1,53 @@
+#include "ZMP.h"
+#include <KrisLibrary/robotics/NewtonEuler.h>
+
+///Utility: returns the center of mass first and second derivatives given joint positions and first and second derivatives.
+///Note: changes the robot's configuration and velocity to q and dq, respectively.
+void GetCOMDerivs(Robot& robot,const Config& q,const Vector& dq,const Vector& ddq,Vector3& cm,Vector3& dcm,Vector3& ddcm,NewtonEulerSolver& ne)
+{
+  robot.UpdateConfig(q);
+  robot.dq = dq;
+  ne.CalcLinkAccel(ddq);
+  cm = robot.GetCOM();
+  dcm.setZero();
+  ddcm.setZero();
+  for(size_t j=0;j<ne.velocities.size();j++) {
+    Vector3 cmofs = robot.links[j].T_World.R*robot.links[j].com;
+    Vector3 vcmj = ne.velocities[j].v + cross(ne.velocities[j].w,cmofs);
+    Vector3 acmj = ne.accelerations[j].v + cross(ne.accelerations[j].w,cmofs) + cross(ne.velocities[j].w,cross(ne.velocities[j].w,cmofs));
+    dcm += robot.links[j].mass*vcmj;
+    ddcm += robot.links[j].mass*acmj;
+  }
+  Real mtotal = robot.GetTotalMass();
+  dcm /= mtotal;
+  ddcm /= mtotal;
+}
+
+///Utility: returns the center of mass first and second derivatives given joint positions and first and second derivatives.
+///Note: changes the robot's configuration and velocity to q and dq, respectively.
+void GetCOMDerivs(Robot& robot,const Config& q,const Vector& dq,const Vector& ddq,Vector3& cm,Vector3& dcm,Vector3& ddcm)
+{
+  NewtonEulerSolver ne(robot);
+  GetCOMDerivs(robot,q,dq,ddq,cm,dcm,ddcm,ne);
+  return;
+}
+
+///Returns the zero-moment point of a robot executing the given trajectory q(t) with first and second derivatives dq and ddq.
+///The ground height is assumed to be 0 by default and gravity is assumed to be 9.8m/s^2.
+Vector2 GetZMP(Robot& robot,const Config& q,const Vector& dq,const Vector& ddq,NewtonEulerSolver& ne,Real groundHeight,Real g)
+{
+  Vector3 cm,dcm,ddcm;
+  GetCOMDerivs(robot,q,dq,ddq,cm,dcm,ddcm,ne);
+  Vector2 zmp;
+  zmp.x = cm.x - (cm.z - groundHeight)/g * ddcm.x;
+  zmp.y = cm.y - (cm.z - groundHeight)/g * ddcm.y;
+  return zmp;
+}
+
+///Returns the zero-moment point of a robot executing the given trajectory q(t) with first and second derivatives dq and ddq.
+///The ground height is assumed to be 0 by default and gravity is assumed to be 9.8m/s^2.
+Vector2 GetZMP(Robot& robot,const Config& q,const Vector& dq,const Vector& ddq,Real groundHeight,Real g)
+{
+  NewtonEulerSolver ne(robot);
+  return GetZMP(robot,q,dq,ddq,ne,groundHeight,g);
+}
