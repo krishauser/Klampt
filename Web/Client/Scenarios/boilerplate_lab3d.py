e180c893acb7aa7ea0f7bd5748540e9e7e62a395
motion <motion@scovel.egr.duke.edu>
2016-10-13 15:08:43 -0400

    Added lab3D

---
+++ Web/Client/Scenarios/boilerplate_lab3d.py
@@ -0,0 +1,127 @@
+import pkg_resources
+pkg_resources.require("klampt==0.6.2")
+from klampt import *
+from klampt.cspace import MotionPlan
+import math
+import sys
+sys.path.append("Web/Server")
+import kviz
+import random
+from klampt import vectorops
+
+world = WorldModel()
+space = None
+start = None
+target = None
+path = None
+roadmap = None
+planner = None
+optimizing = False
+existing_path_lines = []
+existing_ghosts = []
+existing_roadmap_lines = set()
+
+    
+def boilerplate_start():
+    global world,space,start,target,planner,optimizing,path,existing_path_lines,roadmap,existing_roadmap_lines
+
+    #global options that don't get forgotten from instance to instance
+    MotionPlan.setOptions(restart=False,shortcut=False)
+
+    start=stub.start()
+    target=stub.target()
+
+    world = WorldModel()
+    world.readFile("Web/Client/Scenarios/lab3/plan_world.xml")
+    space = stub.SE2ObstacleCSpace(world)
+
+    #TODO: in Problem 4 you should switch the commented-out lines to
+    #decide whether you want a feasible planner or an optimizing planner
+    planner,optimizing = stub.makePlanner(space,start,target)
+    try:
+        planner.setEndpoints(start,target)
+    except:
+        pass
+    
+    path = []
+    roadmap = [],[]
+    kviz._init(world)
+    radius = 0.025
+    
+    print world.robot(0).numLinks()
+    start_ghost = kviz.add_ghost("start")
+    target_ghost = kviz.add_ghost("target")
+    kviz.set_ghost_config(start,"start")
+    kviz.set_ghost_config(target,"target")
+    kviz.set_color(start_ghost,[0,1,0,1])
+    kviz.set_color(target_ghost,[1,0,0,1])
+    existing_roadmap_lines = set()
+    existing_path_lines = []
+    existing_ghosts = []
+    refresh_viz()
+
+def refresh_viz():
+    global start,target,path,existing_path_lines,roadmap,existing_roadmap_lines
+    kviz.update_sphere("robot",start[0],start[1],0)
+    kviz.update_sphere("target",target[0],target[1],0)
+    for i,name in enumerate(existing_path_lines):
+        if i >= len(path):
+            kviz.set_visible(name,False)
+            kviz.set_visible(existing_ghosts[i])
+    for i in xrange(len(path)-1):
+        if i >= len(existing_path_lines):
+            name = "path"+str(i)
+            existing_path_lines.append(name)
+            kviz.add_line(name,path[i][0],path[i][1],0.0,path[i+1][0],path[i+1][1],0.0)
+            kviz.set_visible(name,True)
+            kviz.set_color(name,[0,0,1,1])
+            existing_ghosts.append(kviz.add_ghost("ghost"+str(i)))
+            kviz.set_ghost_config(path[i],"ghost"+str(i))
+        else:
+            name = existing_path_lines[i]
+            kviz.update_line(name,path[i][0],path[i][1],0.0,path[i+1][0],path[i+1][1],0.0)
+            kviz.set_ghost_config(path[i],"ghost"+str(i))
+    if stub.draw_roadmap:
+        V,E = roadmap
+        for i,e in enumerate(E):
+            name = "edge"+str(i)
+            a,b = e
+            if name not in existing_roadmap_lines:
+                existing_roadmap_lines.add(name)
+                kviz.add_line(name,V[a][0],V[a][1],0.0,V[b][0],V[b][1],0.0)
+                kviz.set_color(name,[1,1,0,1])
+            else:
+                kviz.update_line(name,V[a][0],V[a][1],0.0,V[b][0],V[b][1],0.0)
+                kviz.set_visible(name,True)
+
+def boilerplate_advance():
+    global path,optimizing,planner,roadmap
+    if optimizing or not path:
+        t0 = time.time()
+        iters = 0
+        while time.time() - t0 < stub.max_plan_time:
+            planner.planMore(1)
+            iters += 1
+            if iters >= stub.max_plan_iters:
+                break
+        t1 = time.time()
+        V,E = planner.getRoadmap()
+        roadmap = V,E
+        print len(V),"feasible milestones sampled,",len(E),"edges connected",t1-t1,"s"
+        path = planner.getPath()
+        if path == None:
+            path = []
+        refresh_viz()
+
+def boilerplate_keypress(c):
+    global field,start,path
+    if c=='randomize':
+        for i in xrange(10000):
+            q = (random.random(),random.random())
+            if not any(o.contains(q) for o in field.obstacles):
+                break
+        start = q
+        path = [q]
+        refresh_viz()
+
+    
---
+++ Web/Client/Scenarios/code_lab3d.py
@@ -0,0 +1,98 @@
+from klampt import *
+from klampt import vectorops,so2,so3
+from klampt.cspace import CSpace,MotionPlan
+import math
+
+#turn this to False if redrawing becomes too slow
+draw_roadmap = True
+#how much planning time to take every frame, in seconds
+max_plan_time = 0.5
+#how many iterations to take per frame
+max_plan_iters = 100
+
+class SE2ObstacleCSpace(CSpace):
+    """The configuration space being used in Lab 3D.
+    Consists of a RobotModel robot and TerrainModel obstacles.
+
+    Note the much larger size of the space!
+    """
+    def __init__(self,world):
+        CSpace.__init__(self)
+        #set bounds
+        self.bound = [(-5,5),(-5,5),(0,math.pi*2)]
+        #set collision checking resolution
+        self.eps = 1e-1
+        #get the robot (a RobotModel instance)
+        self.robot = world.robot(0)
+        #get obstacles here, these will be TerrainModel instances
+        self.obstacles = [world.terrain(i) for i in xrange(world.numTerrains())]
+    
+    def feasible(self,q):
+        """TODO: Implement this feasibility test.  It is used by the motion planner to
+        determine whether the robot at configuration q is feasible."""
+        #bounds test: we don't care about angle
+        if not CSpace.feasible(self,(q[0],q[1],0)): return False
+        self.robot.setConfig(q)
+        base = self.robot.link(2)
+        for o in self.obstacles:
+            if o.geometry().collides(base.geometry()): return False
+        return True
+
+    def interpolate(self,a,b,u):
+        """TODO: Implement this interpolation function.
+        Inputs:
+        - a: the start configuration
+        - b: the end configuration
+        - u: the interpolation parameter in the range from 0 to 1 
+        Out: the interpolated configuration
+        """
+        return vectorops.interpolate(a,b,u)
+
+    def distance(self,a,b):
+        """TODO: Implement this interpolation function.
+        Inputs:
+        - a: the start configuration
+        - b: the end configuration
+        """
+        return vectorops.distance(a,b)
+
+def makePlanner(space, start, goal):
+    """Creates a MotionPlan object for the given space, start, and goal.
+    Returns (planner,optimizing) where optimizing is True if the planner should
+    continue be run after the first solution path has been found"""
+    #TODO: In lab3c, you should tune these parameters
+    #
+    #This sets a Probabilistic Road Map (PRM) planner that connects
+    #a random point to its 10 nearest neighbors. If knn is set to 0,
+    #the points are connected as long as they lie
+    #within distance 0.1 of one another
+    #MotionPlan.setOptions(type="prm",knn=10,connectionThreshold=1)
+    #This line sets a Rapidly-exploring Random Tree (RRT) planner that
+    #repeatedly extends the tree toward a random point at maximum
+    #distance 0.25.  It uses the bidirectional=True option, which grows
+    #trees from both the start and the goal
+    #MotionPlan.setOptions(type="rrt",connectionThreshold=2.0,perturbationRadius=2.5,bidirectional=True)
+    #MotionPlan.setOptions(type="sbl",connectionThreshold=5.0,gridResolution=1.0,perturbationRadius=1.5,bidirectional=True)
+    optimizing = False
+
+    #Optimizing planners, for use in Lab3D.  Make sure to uncomment optimizing = True below.
+    #This sets the PRM algorithm with shortcutting
+    #MotionPlan.setOptions(type="prm",knn=10,connectionThreshold=1.0,shortcut=True)
+    #This sets the RRT* algorithm
+    #MotionPlan.setOptions(type="rrt*",connectionThreshold=2.0,perturbationRadius=2.5)
+    #This sets a fast-marching method algorithm
+    #MotionPlan.setOptions(type="fmm*")
+    #This sets a random-restart + shortcutting RRT
+    MotionPlan.setOptions(type="rrt",connectionThreshold=2.0,perturbationRadius=2.5,bidirectional=True,restart=True,shortcut=True,restartTermCond="{foundSolution:1,maxIters:1000}")
+    optimizing = True
+
+    #create the planner and set the termination criteria
+    planner = MotionPlan(space)
+    return planner,optimizing
+
+def start():
+    return (-3.5,-3,math.pi/4)
+
+def target():
+    return (3.5,3,math.pi*7/4)
+
