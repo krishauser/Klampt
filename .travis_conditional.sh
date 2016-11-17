eb8f94352e415dec1826f8a72387d12ff386f53c
Alessio Rocchi <alessio.rocchi@iit.it>
2016-11-17 21:29:00 +0100

    Adding an echo to debug travis conditional build

--- .travis_conditional.sh
+++ .travis_conditional.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-if [ $TRAVIS_BRANCH == 'v0.7' ] || [ $TRAVIS_BRANCH == 'travis' ]; then
-  cd Library
-  make deps-unpack
+cd Library
+make deps-unpack
+
+if [ $TRAVIS_BRANCH == 'v0.7' ] || [ $TRAVIS_BRANCH == 'travis' ]; then  
   cd KrisLibrary
   git checkout plan_devel
   cd ..
-else
-  cd Library
-  make deps-unpack
+  echo "Checking out branch plan_devel for KrisLibrary"
 fi
 
-cd
+cd ..
