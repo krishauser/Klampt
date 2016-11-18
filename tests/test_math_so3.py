3e2e85884a7e011fa0fe28e574ab70c6a0719025
Alessio Rocchi <alessio.rocchi@iit.it>
2016-11-18 20:50:41 +0100

    Added initial test for klampt.math.so3

---
+++ tests/test_math_so3.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import unittest
+from klampt.math import so3
+
+class so3Test(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_from_rpy(self):
+        R = so3.from_rpy((0,0,0))
