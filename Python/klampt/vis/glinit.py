42f93ac53b60bf4fec1c84c90703b94966ac98f3
Kris Hauser <kris.hauser@duke.edu>
2017-01-10 14:58:09 -0500

    Turned off GLUTAvailable on GLUTBackend import failure

--- Python/klampt/vis/glinit.py
+++ Python/klampt/vis/glinit.py
@@ -4,25 +4,19 @@ _GLUTAvailable = False
 _GLBackend = None
 
 if not _PyQtAvailable and not _GLUTAvailable:
-    """
-    from OpenGL.GLUT import *
-    _GLUTAvailable = True
-    from glutbackend import GLUTBackend
-    _GLBackend = GLUTBackend()
-    """
     try:
         from PyQt4.QtCore import *
         from PyQt4.QtGui import *
-        _PyQtAvailable = True
         from qtbackend import QtBackend
+        _PyQtAvailable = True
         _GLBackend = QtBackend()
         print "***  klampt.vis: using Qt as the visualization backend  ***"
     except ImportError:
         print "QT is not available... try sudo apt-get install python-qt4 python-qt4-gl"
         try:
             from OpenGL.GLUT import *
-            _GLUTAvailable = True
             from glutbackend import GLUTBackend
+            _GLUTAvailable = True
             _GLBackend = GLUTBackend()
             print "*** klampt.vis: using GLUT as the visualization backend ***"
             print "***      Some functionality may not be available!       ***"
