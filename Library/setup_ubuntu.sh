585f9381f3fe8d147a4cfaad17fec13d14eed3b2
Kris Hauser <Kris Hauser>
2017-03-27 03:02:39 -0400

    Removed unnecessary script

--- Library/setup_ubuntu.sh
+++
@@ -1,8 +0,0 @@
-make unpack-deps
-make deps
-#let the ODE library be loadable from inside this path without installing
-#which might override some other ODE installation 
-echo "Adding local ODE path to ld.so.conf.d"
-echo ${PWD}/ode-0.11.1/ode/src/.libs/ > ode.conf
-sudo mv ode.conf /etc/ld.so.conf.d 
-
