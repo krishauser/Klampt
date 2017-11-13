b6f3921511492976e48442e91285564a3cd63eb8
IML Shared <dukeimlshared@gmail.com>
2017-11-13 12:05:39 -0500

    fixed bounds

--- IO/three.js.cpp
+++ IO/three.js.cpp
@@ -27,7 +27,7 @@ string MakeRandomUUID()
   char alphaNumeric[] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9','+','-','=','$','@','!'};
   //make a 36 char long string
   std::default_random_engine generator;
-  std::uniform_int_distribution<int> distribution(38,125);
+  std::uniform_int_distribution<int> distribution(0,67);
 
   //boost::uuids::uuid uuid = boost::uuids::random_generator()();
   for(int i = 0; i<36; i++){
