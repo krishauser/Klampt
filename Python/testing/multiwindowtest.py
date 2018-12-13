c30e7b51218b43af65fe34ef98c9a2ec9811abf0
Kris Hauser <kris.hauser@duke.edu>
2018-12-12 23:36:15 -0500

    More informative messages

--- Python/testing/multiwindowtest.py
+++ Python/testing/multiwindowtest.py
@@ -2,11 +2,14 @@ from klampt import *
 from klampt import vis
 import time
 
+print ("Tests multiple simultaneous windows.  This requires multithreaded visualization,")
+print ("which doesn't work on Mac")
+
 world1 = WorldModel()
 world1.readFile('../../data/objects/block.obj')
 id1 = vis.createWindow('First')
 def firsthello():
-	print "hello from First"
+	print ("hello from First")
 vis.visualization._vis.add_action(firsthello,"First's action","p")
 vis.add('world1', world1)
 vis.show()
@@ -15,7 +18,7 @@ world2 = WorldModel()
 world2.readFile('../../data/robots/athlete.rob')
 id2 = vis.createWindow('Second')
 def secondhello():
-	print "hello from Second"
+	print ("hello from Second")
 vis.visualization._vis.add_action(secondhello,"Second's action","q")
 vis.add('world2', world2)
 
