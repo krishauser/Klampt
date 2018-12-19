d82db4e06b1a0b1a495ec2920046701caa4f9598
Kris Hauser <kris.hauser@duke.edu>
2018-12-19 16:44:01 -0500

    Bending over backward to support mac 10.6

--- Python/klampt/src/motionplanning.cpp
+++ Python/klampt/src/motionplanning.cpp
@@ -4,6 +4,48 @@
     // earlier than 10.6, C++11 items are in the tr1 namespace
     #include <tr1/memory>
     #define shared_ptr tr1::shared_ptr
+
+    //make_shared is not defined
+    // zero arguments version
+    template <typename T>
+    inline shared_ptr<T> make_shared()
+    {
+      return shared_ptr<T>(new T());
+    }
+
+    // one argument version
+    template <typename T, typename Arg1>
+    inline shared_ptr<T> make_shared(Arg1&& arg1)
+    {
+      return shared_ptr<T>(new T(std::forward<Arg1>(arg1)));
+    }
+
+    // two arguments version
+    template <typename T, typename Arg1, typename Arg2>
+    inline shared_ptr<T> make_shared(Arg1&& arg1, Arg2&& arg2)
+    {
+      return shared_ptr<T>(new T(std::forward<Arg1>(arg1),
+                                 std::forward<Arg2>(arg2)));
+    }
+
+    // three arguments version
+    template <typename T, typename Arg1, typename Arg2, typename Arg3>
+    inline shared_ptr<T> make_shared(Arg1&& arg1, Arg2&& arg2, Arg3&& arg3)
+    {
+      return shared_ptr<T>(new T(std::forward<Arg1>(arg1),
+                                 std::forward<Arg2>(arg2),
+                                 std::forward<Arg3>(arg3)));
+    }
+
+    // four arguments version
+    template <typename T, typename Arg1, typename Arg2, typename Arg3, typename Arg3>
+    inline shared_ptr<T> make_shared(Arg1&& arg1, Arg2&& arg2, Arg3&& arg3, Arg4&& arg4)
+    {
+      return shared_ptr<T>(new T(std::forward<Arg1>(arg1),
+                                 std::forward<Arg2>(arg2),
+                                 std::forward<Arg3>(arg3),
+                                 std::forward<Arg4>(arg4)));
+    }
   #endif 
 #endif //Mac Python 10.6 
 
--- Python/klampt/src/robotsim.cpp
+++ Python/klampt/src/robotsim.cpp
@@ -4,6 +4,29 @@
     // earlier than 10.6, C++11 items are in the tr1 namespace
     #include <tr1/memory>
     #define shared_ptr tr1::shared_ptr
+
+    //make_shared is not defined
+    // zero arguments version
+    template <typename T>
+    inline shared_ptr<T> make_shared()
+    {
+      return shared_ptr<T>(new T());
+    }
+
+    // one argument version
+    template <typename T, typename Arg1>
+    inline shared_ptr<T> make_shared(Arg1&& arg1)
+    {
+      return shared_ptr<T>(new T(std::forward<Arg1>(arg1)));
+    }
+
+    // two arguments version
+    template <typename T, typename Arg1, typename Arg2>
+    inline shared_ptr<T> make_shared(Arg1&& arg1, Arg2&& arg2)
+    {
+      return shared_ptr<T>(new T(std::forward<Arg1>(arg1),
+                                 std::forward<Arg2>(arg2)));
+    }
   #endif 
 #endif //Mac Python 10.6 
 
