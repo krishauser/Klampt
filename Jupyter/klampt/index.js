7d6270f31a93bd1cfbfe73faf89208b013fb8298
Kris Hauser <kris.hauser@duke.edu>
2019-04-11 13:19:55 -0400

    Fixed missing reference to json file

--- Jupyter/klampt/index.js
+++ Jupyter/klampt/index.js
@@ -1,3 +1,3 @@
 // Export widget models and views, and the npm package version number.
 module.exports = require('./main.js');
-module.exports['version'] = require('../package.json').version;
+module.exports['version'] = '0.1.0';
