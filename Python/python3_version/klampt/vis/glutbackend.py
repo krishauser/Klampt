cc613be80645530a0a69f44101f886e841e0d1fd
Kris Hauser <kris.hauser@duke.edu>
2020-01-17 01:49:55 -0500

    Fixed some backend problems with GLUT modifiers

--- Python/python3_version/klampt/vis/glutbackend.py
+++ Python/python3_version/klampt/vis/glutbackend.py
@@ -82,8 +82,8 @@ class GLUTWindow:
         glutKeyboardUpFunc (glutsafe(program.keyboardupfunc,update_modifiers=True))
         glutSpecialFunc (glutsafe(self._specialfunc,update_modifiers=True))
         glutSpecialUpFunc (glutsafe(self._specialupfunc,update_modifiers=True))
-        glutMotionFunc (glutsafe(self._motionfunc,update_modifiers=True))
-        glutPassiveMotionFunc (glutsafe(self._motionfunc,update_modifiers=True))
+        glutMotionFunc (glutsafe(self._motionfunc,update_modifiers=False))
+        glutPassiveMotionFunc (glutsafe(self._motionfunc,update_modifiers=False))
         glutMouseFunc (glutsafe(self._mousefunc,update_modifiers=True))
         glutDisplayFunc (glutsafe(self._displayfunc))
         glutIdleFunc(glutsafe(program.idlefunc))
@@ -188,9 +188,9 @@ class GLUTWindow:
         modifiers = glutGetModifiers()
         if modifiers & GLUT_ACTIVE_CTRL:
             m.append('ctrl')
-        elif modifiers & GLUT_ACTIVE_SHIFT:
+        if modifiers & GLUT_ACTIVE_SHIFT:
             m.append('shift')
-        elif modifiers & GLUT_ACTIVE_ALT:
+        if modifiers & GLUT_ACTIVE_ALT:
             m.append('alt')
         self.modifierList = m
 
@@ -215,7 +215,6 @@ class GLUTWindow:
         self.lastx = x
         self.lasty = y
 
-
     def _specialfunc(self,c,x,y):
         if c in keymap:
             self.program.keyboardfunc(keymap[c],x,y)
@@ -236,6 +235,7 @@ class GLUTWindow:
     def _closefunc(self):
         self.program.closefunc()
         self.program.window = None
+
 
 class GLUTBackend:
     """A basic OpenGL program using GLUT.  Set up your GLProgramInterface class,
