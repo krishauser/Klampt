820534ee80586659f386c4b17c990c9ce8a5b20c
Kris Hauser <kris.hauser@duke.edu>
2020-05-19 01:08:11 -0400

    Now SimRobotSensor is included by default

--- Python/klampt/__init__.py
+++ Python/klampt/__init__.py
@@ -3,7 +3,7 @@ import atexit
 atexit.register(destroy)
 
 __all__ = ['WorldModel','RobotModel','RobotModelLink','RigidObjectModel','TerrainModel','Mass','ContactParameters',
-           'SimRobotController','SimBody','Simulator',
+           'SimRobotController','SimRobotSensor','SimBody','Simulator',
            'Geometry3D','Appearance','DistanceQuerySettings','DistanceQueryResult','TriangleMesh','PointCloud','GeometricPrimitive','VolumeGrid',
            'IKObjective','IKSolver','GeneralizedIKObjective','GeneralizedIKSolver',
            'model','math','io','plan','sim']
