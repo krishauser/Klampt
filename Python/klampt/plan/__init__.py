712138bc4ffc50e095d0d0993d27aa68460429cc
paperstiger <gaotang2@illinois.edu>
2020-06-24 00:06:35 -0500

    add kinetrajopt to klampt

--- Python/klampt/plan/__init__.py
+++ Python/klampt/plan/__init__.py
@@ -1,7 +1,8 @@
-__all__ = ['cspace','robotcspace','robotplanning']
+__all__ = ['cspace','robotcspace','robotplanning', 'kinetrajopt']
 from . import motionplanning
 from . import cspace
 from . import robotcspace
 from . import robotplanning
+from . import kinetrajopt
 import atexit
 atexit.register(motionplanning.destroy)
