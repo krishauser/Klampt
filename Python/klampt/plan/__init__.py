6eda90a9af936f18260217e1b10e72aedf25203f
Kris Hauser <kris.hauser@duke.edu>
2020-07-02 04:37:22 -0400

    Not importing kinetrajopt by default

--- Python/klampt/plan/__init__.py
+++ Python/klampt/plan/__init__.py
@@ -1,8 +1,7 @@
-__all__ = ['cspace','robotcspace','robotplanning', 'kinetrajopt']
+__all__ = ['cspace','robotcspace','robotplanning']
 from . import motionplanning
 from . import cspace
 from . import robotcspace
 from . import robotplanning
-from . import kinetrajopt
 import atexit
 atexit.register(motionplanning.destroy)
