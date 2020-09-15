2eadde4317a617654a630d7ed18ae0f2ee7d5b5d
Kris Hauser <kris.hauser@duke.edu>
2020-09-15 02:10:50 -0400

    Improved error messages when using kinetrajopt

--- Python/klampt/plan/kinetrajopt/__init__.py
+++ Python/klampt/plan/kinetrajopt/__init__.py
@@ -1,7 +1,12 @@
 try:
     import cvxpy
-    import autograd
-    from .utils import CostInterface, ConstrInterface
-    from .kinetrajopt import TrajOptConfig, KineTrajOpt
-except:
-    print("Cannot import cvxpy and autograd, kinedynopt is not imported")
+    try:
+        import autograd
+        from .utils import CostInterface, ConstrInterface
+        from .kinetrajopt import TrajOptConfig, KineTrajOpt
+    except ImportError:
+        print('kinetrajopt is not supported because autograd cannot be imported. Try "pip install autograd"')
+        raise
+except ImportError:
+    print('kinetrajopt is not supported because cvxpy cannot be imported. Try "pip install cvxpy"')
+    raise
