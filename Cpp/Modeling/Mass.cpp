11d5711447148730d306714650d6d791f18a0407
Kris Hauser <kris.hauser@duke.edu>
2020-11-27 01:19:11 -0500

    Fixed big problem with inertia estimation for TriangleMesh types, added ability to shift mass between surface and volume

--- Cpp/Modeling/Mass.cpp
+++ Cpp/Modeling/Mass.cpp
@@ -27,31 +27,58 @@ inline void AddOuterProduct(Matrix3& m,const Vector3& x,const Vector3& y)
 
 
 //first and second moments integrated over mesh
-Vector3 CenterOfMass(const TriMesh& mesh)
+Vector3 CenterOfMass(const TriMesh& mesh,Real surfaceFraction)
 {
   Triangle3D tri;
   Vector3 center(Zero);
-  if(mesh.tris.empty()) return center;
-  Real sumArea=Zero;
-  for(size_t i=0;i<mesh.tris.size();i++) {
-    mesh.GetTriangle(i,tri);
-    Real area=tri.area();
-    center.madd(tri.a+tri.b+tri.c,area*third);
-    sumArea += area;
+  if(mesh.tris.empty()) {
+    return center;
   }
-  Assert(sumArea != Zero);
-  center /= sumArea;
+
+  if(surfaceFraction != 0) {
+    Real sumArea=Zero;
+    for(size_t i=0;i<mesh.tris.size();i++) {
+      mesh.GetTriangle(i,tri);
+      Real area=tri.area();
+      center.madd(tri.a+tri.b+tri.c,area*third);
+      sumArea += area;
+    }
+    Assert(sumArea != Zero);
+    center *= surfaceFraction/sumArea;
+  }
+
+  if(surfaceFraction != 1) {
+    //use tetrahedral integration
+    Vector3 center2(Zero);
+    Vector3 centroid(Zero);
+    for(size_t i=0;i<mesh.verts.size();i++)
+      centroid += mesh.verts[i];
+    centroid /= mesh.verts.size();
+    Real sumVolume=Zero;
+    Real sixth = 1.0/6.0;
+    for(const auto& t:mesh.tris) {
+      Matrix3 mat;
+      mat.setCol1(mesh.verts[t.a] - centroid);
+      mat.setCol2(mesh.verts[t.b] - centroid);
+      mat.setCol3(mesh.verts[t.c] - centroid);
+      Real volume = mat.determinant()*sixth;
+      center2.madd(mesh.verts[t.a]+mesh.verts[t.b]+mesh.verts[t.c]+centroid,volume*0.25);
+      sumVolume += volume;
+    }
+    center2 *= (1.0-surfaceFraction)/sumVolume;
+    center += center2;
+  }
   return center;
 }
 
-Vector3 CenterOfMass(const Math3D::GeometricPrimitive3D& g)
+Vector3 CenterOfMass(const Math3D::GeometricPrimitive3D& g,Real surfaceFraction)
 {
   AABB3D bbox = g.GetAABB();
   return (bbox.bmin + bbox.bmax)*0.5;
 }
 
 
-Vector3 CenterOfMass(const Meshing::PointCloud3D& pc)
+Vector3 CenterOfMass(const Meshing::PointCloud3D& pc,Real surfaceFraction)
 {
   Vector3 center(Zero);
   for(size_t i=0;i<pc.points.size();i++)
@@ -59,7 +86,7 @@ Vector3 CenterOfMass(const Meshing::PointCloud3D& pc)
   return center / pc.points.size();
 }
 
-Vector3 CenterOfMass(const Meshing::VolumeGrid& grid)
+Vector3 CenterOfMass(const Meshing::VolumeGrid& grid,Real surfaceFraction)
 {
   Vector3 mean(Zero);
   Real sum=0;
@@ -76,60 +103,113 @@ Vector3 CenterOfMass(const Meshing::VolumeGrid& grid)
 }
 
 
-Vector3 CenterOfMass(const vector<AnyGeometry3D>& group)
+Vector3 CenterOfMass(const vector<AnyGeometry3D>& group,Real surfaceFraction)
 {
   Vector3 sum(0.0);
   if(group.empty()) return sum;
   for(size_t i=0;i<group.size();i++)
-    sum += CenterOfMass(group[i]);
+    sum += CenterOfMass(group[i],surfaceFraction);
   return sum / group.size();
 }
 
 
-Vector3 CenterOfMass(const AnyGeometry3D& geom)
+Vector3 CenterOfMass(const AnyGeometry3D& geom,Real surfaceFraction)
 {
   switch(geom.type) {
   case AnyGeometry3D::Primitive:
-    return CenterOfMass(geom.AsPrimitive());
+    return CenterOfMass(geom.AsPrimitive(),surfaceFraction);
   case AnyGeometry3D::TriangleMesh:
-    return CenterOfMass(geom.AsTriangleMesh());
+    return CenterOfMass(geom.AsTriangleMesh(),surfaceFraction);
   case AnyGeometry3D::PointCloud:
-    return CenterOfMass(geom.AsPointCloud());
+    return CenterOfMass(geom.AsPointCloud(),surfaceFraction);
   case AnyGeometry3D::ImplicitSurface:
-    return CenterOfMass(geom.AsImplicitSurface());
+    return CenterOfMass(geom.AsImplicitSurface(),surfaceFraction);
   case AnyGeometry3D::Group:
-    return CenterOfMass(geom.AsGroup());
+    return CenterOfMass(geom.AsGroup(),surfaceFraction);
+  case AnyGeometry3D::ConvexHull:
+    {
+      AnyGeometry3D mesh;
+      geom.Convert(AnyGeometry3D::TriangleMesh,mesh);
+      return CenterOfMass(mesh.AsTriangleMesh(),surfaceFraction);
+    }
+    break;
   }
   return Vector3(0.0);
 }
 
-Matrix3 Covariance(const TriMesh& mesh,const Vector3& center)
+Matrix3 Covariance(const TriMesh& mesh,const Vector3& center,Real surfaceFraction)
 {
   Triangle3D tri;
   Real sumArea=Zero;
   Matrix3 A,temp;
   A.setZero();
-  if(mesh.tris.empty()) return A;
-  for(size_t i=0;i<mesh.tris.size();i++) {
-    mesh.GetTriangle(i,tri);
-    tri.a-=center;
-    tri.b-=center;
-    tri.c-=center;
-    temp.setZero();
-    AddOuterProduct(temp,tri.a,Two*tri.a+tri.b+tri.c);
-    AddOuterProduct(temp,tri.b,Two*tri.b+tri.a+tri.c);
-    AddOuterProduct(temp,tri.c,Two*tri.c+tri.a+tri.b);
-    Real area = tri.area();
-    temp *= area;
-    A += temp;
-    sumArea += area;
+  if(mesh.tris.empty()) {
+    return A;
   }
-  Assert(sumArea != Zero);
-  A *= 1.0/sumArea;
+  if(surfaceFraction != 0) {
+    for(size_t i=0;i<mesh.tris.size();i++) {
+      mesh.GetTriangle(i,tri);
+      tri.a-=center;
+      tri.b-=center;
+      tri.c-=center;
+      temp.setZero();
+      AddOuterProduct(temp,tri.a,Two*tri.a+tri.b+tri.c);
+      AddOuterProduct(temp,tri.b,Two*tri.b+tri.a+tri.c);
+      AddOuterProduct(temp,tri.c,Two*tri.c+tri.a+tri.b);
+      Real area = tri.area();
+      temp *= area/12.0;
+      A += temp;
+      sumArea += area;
+    }
+    Assert(sumArea != Zero);
+    A *= surfaceFraction/sumArea;
+  }
+  if(surfaceFraction != 1) {
+    Matrix3 A2;
+    Vector3 diag(Zero);
+    Vector3 offd(Zero);
+    Real sumVolume=0;
+    Real sixth = 1.0/6.0;
+    for(size_t i=0;i<mesh.tris.size();i++) {
+      mesh.GetTriangle(i,tri);
+      tri.a-=center;
+      tri.b-=center;
+      tri.c-=center;
+      Matrix3 mat;
+      mat.setCol1(tri.a);
+      mat.setCol2(tri.b);
+      mat.setCol3(tri.c);
+      Real volume = mat.determinant()*sixth;
+      sumVolume += volume;
+      for(int j=0;j < 3;j++)
+      {
+        int j1=(j+1)%3;   
+        int j2=(j+2)%3;   
+        diag[j] += (tri.a[j]*tri.b[j] + tri.b[j]*tri.c[j] + tri.c[j]*tri.a[j] + 
+                    tri.a[j]*tri.a[j] + tri.b[j]*tri.b[j] + tri.c[j]*tri.c[j]  ) *volume; // divide by 60 later
+        offd[j] += ((tri.a[j1]*tri.b[j2]  + tri.b[j1]*tri.c[j2]  + tri.c[j1]*tri.a[j2]  +
+                    tri.a[j1]*tri.c[j2]  + tri.b[j1]*tri.a[j2]  + tri.c[j1]*tri.b[j2])*0.5  +
+                    tri.a[j1]*tri.a[j2]+ tri.b[j1]*tri.b[j2]+ tri.c[j1]*tri.c[j2] ) *volume; // divide by 60 later
+      }
+    }
+    diag /= sumVolume*(10.0);
+    offd /= sumVolume*(10.0);
+    A2(0,0) = diag.x;
+    A2(1,1) = diag.y;
+    A2(2,2) = diag.z;
+    A2(0,1) = offd.z;
+    A2(1,0) = offd.z;
+    A2(1,2) = offd.x;
+    A2(2,1) = offd.x;
+    A2(0,2) = offd.y;
+    A2(2,0) = offd.y;
+    A2 *= (1.0-surfaceFraction);
+    A += A2;
+  }
   return A;
 }
 
-Matrix3 Covariance(const PointCloud3D& pc,const Vector3& center)
+Matrix3 Covariance(const PointCloud3D& pc,const Vector3& center,Real surfaceFraction)
 {
   Matrix3 A;
   A.setZero();
@@ -141,7 +221,7 @@ Matrix3 Covariance(const PointCloud3D& pc,const Vector3& center)
   return A;
 }
 
-Matrix3 Covariance(const VolumeGrid& grid,const Vector3& center)
+Matrix3 Covariance(const VolumeGrid& grid,const Vector3& center,Real surfaceFraction)
 {
   Matrix3 cov(Zero);
   Real sum=0;
@@ -161,7 +241,7 @@ Matrix3 Covariance(const VolumeGrid& grid,const Vector3& center)
 }
 
 
-Matrix3 Covariance(const GeometricPrimitive3D& g,const Vector3& center)
+Matrix3 Covariance(const GeometricPrimitive3D& g,const Vector3& center,Real surfaceFraction)
 {
   AABB3D bbox = g.GetAABB();
   Matrix3 res(0.0);
@@ -172,35 +252,42 @@ Matrix3 Covariance(const GeometricPrimitive3D& g,const Vector3& center)
 }
 
 
-Matrix3 Covariance(const vector<AnyGeometry3D>& group,const Vector3& center)
+Matrix3 Covariance(const vector<AnyGeometry3D>& group,const Vector3& center,Real surfaceFraction)
 {
   Matrix3 res(0.0); 
   for(size_t i=0;i<group.size();i++) 
-    res += Covariance(group[i],center);
+    res += Covariance(group[i],center,surfaceFraction);
   return res;
 }
 
 
-Matrix3 Covariance(const AnyGeometry3D& geom,const Vector3& center)
+Matrix3 Covariance(const AnyGeometry3D& geom,const Vector3& center,Real surfaceFraction)
 {
   switch(geom.type) {
   case AnyGeometry3D::Primitive:
-    return Covariance(geom.AsPrimitive(),center);
+    return Covariance(geom.AsPrimitive(),center,surfaceFraction);
   case AnyGeometry3D::TriangleMesh:
-    return Covariance(geom.AsTriangleMesh(),center);
+    return Covariance(geom.AsTriangleMesh(),center,surfaceFraction);
   case AnyGeometry3D::PointCloud:
-    return Covariance(geom.AsPointCloud(),center);
+    return Covariance(geom.AsPointCloud(),center,surfaceFraction);
   case AnyGeometry3D::ImplicitSurface:
-    return Covariance(geom.AsImplicitSurface(),center);
+    return Covariance(geom.AsImplicitSurface(),center,surfaceFraction);
   case AnyGeometry3D::Group:
-    return Covariance(geom.AsGroup(),center);
+    return Covariance(geom.AsGroup(),center,surfaceFraction);
+  case AnyGeometry3D::ConvexHull:
+    {
+      AnyGeometry3D mesh;
+      geom.Convert(AnyGeometry3D::TriangleMesh,mesh);
+      return Covariance(mesh.AsTriangleMesh(),surfaceFraction);
+    }
+    break;
   }
   return Matrix3();
 }
 
-Matrix3 Inertia(const Math3D::GeometricPrimitive3D& geom,const Vector3& center,Real mass)
+Matrix3 Inertia(const Math3D::GeometricPrimitive3D& geom,const Vector3& center,Real mass,Real surfaceFraction)
 {
-  Matrix3 cov=Covariance(geom,center);
+  Matrix3 cov=Covariance(geom,center,surfaceFraction);
   Matrix3 H;
   H.setNegative(cov);
   H(0,0) = cov(1,1)+cov(2,2);
@@ -211,9 +298,9 @@ Matrix3 Inertia(const Math3D::GeometricPrimitive3D& geom,const Vector3& center,R
 }
 
 
-Matrix3 Inertia(const TriMesh& mesh,const Vector3& center,Real mass)
+Matrix3 Inertia(const TriMesh& mesh,const Vector3& center,Real mass,Real surfaceFraction)
 {
-  Matrix3 cov=Covariance(mesh,center);
+  Matrix3 cov=Covariance(mesh,center,surfaceFraction);
   Matrix3 H;
   H.setNegative(cov);
   H(0,0) = cov(1,1)+cov(2,2);
@@ -223,9 +310,9 @@ Matrix3 Inertia(const TriMesh& mesh,const Vector3& center,Real mass)
   return H;
 }
 
-Matrix3 Inertia(const PointCloud3D& mesh,const Vector3& center,Real mass)
+Matrix3 Inertia(const PointCloud3D& mesh,const Vector3& center,Real mass,Real surfaceFraction)
 {
-  Matrix3 cov=Covariance(mesh,center);
+  Matrix3 cov=Covariance(mesh,center,surfaceFraction);
   Matrix3 H;
   H.setNegative(cov);
   H(0,0) = cov(1,1)+cov(2,2);
@@ -235,22 +322,9 @@ Matrix3 Inertia(const PointCloud3D& mesh,const Vector3& center,Real mass)
   return H;
 }
 
-Matrix3 Inertia(const VolumeGrid& mesh,const Vector3& center,Real mass)
+Matrix3 Inertia(const VolumeGrid& mesh,const Vector3& center,Real mass,Real surfaceFraction)
 {
-  Matrix3 cov=Covariance(mesh,center);
-  Matrix3 H;
-  H.setNegative(cov);
-  H(0,0) = cov(1,1)+cov(2,2);
-  H(1,1) = cov(0,0)+cov(2,2);
-  H(2,2) = cov(0,0)+cov(1,1);
-  H *= mass;
-  return H;
-}
-
-
-Matrix3 Inertia(const vector<AnyGeometry3D>& group,const Vector3& center,Real mass)
-{
-  Matrix3 cov=Covariance(group,center);
+  Matrix3 cov=Covariance(mesh,center,surfaceFraction);
   Matrix3 H;
   H.setNegative(cov);
   H(0,0) = cov(1,1)+cov(2,2);
@@ -261,20 +335,40 @@ Matrix3 Inertia(const vector<AnyGeometry3D>& group,const Vector3& center,Real ma
 }
 
 
+Matrix3 Inertia(const vector<AnyGeometry3D>& group,const Vector3& center,Real mass,Real surfaceFraction)
+{
+  Matrix3 cov=Covariance(group,center,surfaceFraction);
+  Matrix3 H;
+  H.setNegative(cov);
+  H(0,0) = cov(1,1)+cov(2,2);
+  H(1,1) = cov(0,0)+cov(2,2);
+  H(2,2) = cov(0,0)+cov(1,1);
+  H *= mass;
+  return H;
+}
 
-Matrix3 Inertia(const AnyGeometry3D& geom,const Vector3& center,Real mass)
+
+
+Matrix3 Inertia(const AnyGeometry3D& geom,const Vector3& center,Real mass,Real surfaceFraction)
 {
   switch(geom.type) {
   case AnyGeometry3D::Primitive:
-    return Inertia(geom.AsPrimitive(),center,mass);
+    return Inertia(geom.AsPrimitive(),center,mass,surfaceFraction);
   case AnyGeometry3D::TriangleMesh:
-    return Inertia(geom.AsTriangleMesh(),center,mass);
+    return Inertia(geom.AsTriangleMesh(),center,mass,surfaceFraction);
   case AnyGeometry3D::PointCloud:
-    return Inertia(geom.AsPointCloud(),center,mass);
+    return Inertia(geom.AsPointCloud(),center,mass,surfaceFraction);
   case AnyGeometry3D::ImplicitSurface:
-    return Inertia(geom.AsImplicitSurface(),center,mass);
+    return Inertia(geom.AsImplicitSurface(),center,mass,surfaceFraction);
   case AnyGeometry3D::Group:
-    return Inertia(geom.AsGroup(),center,mass);
+    return Inertia(geom.AsGroup(),center,mass,surfaceFraction);
+  case AnyGeometry3D::ConvexHull:
+    {
+      AnyGeometry3D mesh;
+      geom.Convert(AnyGeometry3D::TriangleMesh,mesh);
+      return Inertia(mesh.AsTriangleMesh(),surfaceFraction);
+    }
+    break;
   }
   return Matrix3();
 }
--- Cpp/Modeling/Mass.h
+++ Cpp/Modeling/Mass.h
@@ -7,36 +7,36 @@ using namespace Math3D;
 /** @addtogroup Modeling */
 /*@{*/
 
-///Computes the first moment integrated over the mesh (assuming the mesh
-///is a hollow shell)
-Vector3 CenterOfMass(const Meshing::TriMesh& mesh);
+///Computes the first moment integrated over the mesh.  surfaceFraction indicates how much
+///of the mass is concentrated at mesh considered as a hollow shell vs a solid.
+Vector3 CenterOfMass(const Meshing::TriMesh& mesh,Real surfaceFraction=1.0);
 ///Computes the first moment integrated over the point cloud
-Vector3 CenterOfMass(const Meshing::PointCloud3D& pc);
+Vector3 CenterOfMass(const Meshing::PointCloud3D& pc,Real surfaceFraction=1.0);
 ///Computes the first moment integrated over the volume grid (assuming < 0 
 ///is the interior)
-Vector3 CenterOfMass(const Meshing::VolumeGrid& grid);
+Vector3 CenterOfMass(const Meshing::VolumeGrid& grid,Real surfaceFraction=1.0);
 ///Computes the first moment integrated over the geometry
-Vector3 CenterOfMass(const Geometry::AnyGeometry3D& geom);
+Vector3 CenterOfMass(const Geometry::AnyGeometry3D& geom,Real surfaceFraction=1.0);
 
 ///Computes the second moment integrated over the geometry
-Matrix3 Covariance(const Meshing::TriMesh& mesh,const Vector3& center);
-Matrix3 Covariance(const Meshing::PointCloud3D& pc,const Vector3& center);
-Matrix3 Covariance(const Meshing::VolumeGrid& vol,const Vector3& center);
-Matrix3 Covariance(const Geometry::AnyGeometry3D& geom,const Vector3& center);
-inline Matrix3 Covariance(const Meshing::TriMesh& mesh) { return Covariance(mesh,CenterOfMass(mesh)); }
-inline Matrix3 Covariance(const Meshing::PointCloud3D& mesh) { return Covariance(mesh,CenterOfMass(mesh)); }
-inline Matrix3 Covariance(const Meshing::VolumeGrid& mesh) { return Covariance(mesh,CenterOfMass(mesh)); }
-inline Matrix3 Covariance(const Geometry::AnyGeometry3D& mesh) { return Covariance(mesh,CenterOfMass(mesh)); }
+Matrix3 Covariance(const Meshing::TriMesh& mesh,const Vector3& center,Real surfaceFraction=1.0);
+Matrix3 Covariance(const Meshing::PointCloud3D& pc,const Vector3& center,Real surfaceFraction=1.0);
+Matrix3 Covariance(const Meshing::VolumeGrid& vol,const Vector3& center,Real surfaceFraction=1.0);
+Matrix3 Covariance(const Geometry::AnyGeometry3D& geom,const Vector3& center,Real surfaceFraction=1.0);
+inline Matrix3 Covariance(const Meshing::TriMesh& mesh,Real surfaceFraction=1.0) { return Covariance(mesh,CenterOfMass(mesh,surfaceFraction),surfaceFraction); }
+inline Matrix3 Covariance(const Meshing::PointCloud3D& mesh,Real surfaceFraction=1.0) { return Covariance(mesh,CenterOfMass(mesh,surfaceFraction),surfaceFraction); }
+inline Matrix3 Covariance(const Meshing::VolumeGrid& mesh,Real surfaceFraction=1.0) { return Covariance(mesh,CenterOfMass(mesh,surfaceFraction),surfaceFraction); }
+inline Matrix3 Covariance(const Geometry::AnyGeometry3D& mesh,Real surfaceFraction=1.0) { return Covariance(mesh,CenterOfMass(mesh,surfaceFraction),surfaceFraction); }
 
 ///Computes the inertia tensor by integrating over the mesh
-Matrix3 Inertia(const Meshing::TriMesh& mesh,const Vector3& center,Real mass);
-Matrix3 Inertia(const Meshing::PointCloud3D& mesh,const Vector3& center,Real mass);
-Matrix3 Inertia(const Meshing::VolumeGrid& mesh,const Vector3& center,Real mass);
-Matrix3 Inertia(const Geometry::AnyGeometry3D& mesh,const Vector3& center,Real mass);
-inline Matrix3 Inertia(const Meshing::TriMesh& mesh,Real mass) { return Inertia(mesh,CenterOfMass(mesh),mass); }
-inline Matrix3 Inertia(const Meshing::PointCloud3D& mesh,Real mass) { return Inertia(mesh,CenterOfMass(mesh),mass); }
-inline Matrix3 Inertia(const Meshing::VolumeGrid& mesh,Real mass) { return Inertia(mesh,CenterOfMass(mesh),mass); }
-inline Matrix3 Inertia(const Geometry::AnyGeometry3D& mesh,Real mass) { return Inertia(mesh,CenterOfMass(mesh),mass); }
+Matrix3 Inertia(const Meshing::TriMesh& mesh,const Vector3& center,Real mass,Real surfaceFraction=1.0);
+Matrix3 Inertia(const Meshing::PointCloud3D& mesh,const Vector3& center,Real mass,Real surfaceFraction=1.0);
+Matrix3 Inertia(const Meshing::VolumeGrid& mesh,const Vector3& center,Real mass,Real surfaceFraction=1.0);
+Matrix3 Inertia(const Geometry::AnyGeometry3D& mesh,const Vector3& center,Real mass,Real surfaceFraction=1.0);
+inline Matrix3 Inertia(const Meshing::TriMesh& mesh,Real mass,Real surfaceFraction=1.0) { return Inertia(mesh,CenterOfMass(mesh,surfaceFraction),mass,surfaceFraction); }
+inline Matrix3 Inertia(const Meshing::PointCloud3D& mesh,Real mass,Real surfaceFraction=1.0) { return Inertia(mesh,CenterOfMass(mesh,surfaceFraction),mass,surfaceFraction); }
+inline Matrix3 Inertia(const Meshing::VolumeGrid& mesh,Real mass,Real surfaceFraction=1.0) { return Inertia(mesh,CenterOfMass(mesh,surfaceFraction),mass,surfaceFraction); }
+inline Matrix3 Inertia(const Geometry::AnyGeometry3D& mesh,Real mass,Real surfaceFraction=1.0) { return Inertia(mesh,CenterOfMass(mesh,surfaceFraction),mass,surfaceFraction); }
 
 ///Computes the first moment integrated over the solid inside the mesh
 ///using a grid approximation
