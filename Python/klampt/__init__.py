b034394d987515c222c6cabf3d6c98dbfbc43e3c
Kris Hauser <kris.hauser@duke.edu>
2020-12-21 00:29:58 -0500

    Added version numbers

--- Python/klampt/__init__.py
+++ Python/klampt/__init__.py
@@ -1,12 +1,13 @@
-from .robotsim import *
-import atexit
-atexit.register(destroy)
-
 __all__ = ['WorldModel','RobotModel','RobotModelLink','RigidObjectModel','TerrainModel','Mass','ContactParameters',
            'SimRobotController','SimRobotSensor','SimBody','SimJoint','Simulator',
            'Geometry3D','Appearance','DistanceQuerySettings','DistanceQueryResult','ContactQueryResult',
            'TriangleMesh','PointCloud','GeometricPrimitive','ConvexHull','VolumeGrid',
            'IKObjective','IKSolver','GeneralizedIKObjective','GeneralizedIKSolver',
            'model','math','io','plan','sim','vis','control']
+__version__ = '0.8.5'
+
+from .robotsim import *
+import atexit
+atexit.register(destroy)
 
 
