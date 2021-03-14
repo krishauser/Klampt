5aa95f178dfce3344523b453b82bc6baceefb64c
Kris Hauser <kkhauser@illinois.edu>
2021-03-14 16:53:49 -0400

    Refined interfaces to provide options for cartesian control

--- Python/klampt/control/robotinterface.py
+++ Python/klampt/control/robotinterface.py
@@ -75,6 +75,28 @@ class RobotInterfaceBase(object):
     passes commands to a Klamp't physics simulator.
 
 
+    **Cartesian control**
+
+    Cartesian position, velocity, and force control may be implemented. 
+    The interpretation of these arguments is implementation-dependent, but 
+    we support two standard modes: 6DOF position/orientation and 3DOF
+    position-only mode. 
+    
+    The reference frame for these quantities is specified by a ``frame``
+    argument, which is 'world' by default:
+    
+    * 'world': The world frame.  Equal to 'base' for a top-level robot, but
+       differs if this interface refers to a part of a larger robot.
+    * 'base': The base of this part.  Equal to 'world' for a top-level robot,
+       but differs if this interface refers to a part of a larger robot.
+    * 'end effector': relative to the origin of the end effector frame.
+    * 'tool': relative to the current tool frame (the tool is an offset from
+       the end effector frame)
+
+    Note that an interface might only support commands to one type of frame,
+    and raise NotImplementedError() for other frames.
+
+
     Attributes:
         properties (dict): a dict from string key to property value. Application
             dependent. Examples may include:
@@ -215,6 +237,176 @@ class RobotInterfaceBase(object):
         """Returns the clock time of the last sensor update."""
         raise NotImplementedError()
 
+    def status(self,part=None,joint_idx=None):
+        """Returns a status string for the given part / joint.  'ok' means
+        everything is OK."""
+        if part is not None or joint_idx is not None:
+            return self.getPartController(part,joint_idx).status()
+        return 'ok'
+
+    def isMoving(self,part=None,joint_idx=None):
+        """Returns true if the part / joint are currently moving"""
+        if part is not None or joint_idx is not None:
+            return self.getPartController(part,joint_idx).isMoving()
+        raise NotImplementedError()
+
+    def sensedPosition(self):
+        """Retrieves the currently sensed joint position. 
+        """
+        raise NotImplementedError()
+
+    def sensedVelocity(self):
+        """Retrieves the currently sensed joint velocity. 
+        """
+        raise NotImplementedError()
+
+    def sensedTorque(self):
+        """Retrieves the currently sensed joint torque. 
+        """
+        raise NotImplementedError()
+
+    def commandedPosition(self):
+        """Retrieves the currently commanded joint position. 
+        """
+        raise NotImplementedError()
+
+    def commandedVelocity(self):
+        """Retrieves the currently commanded joint velocity. 
+        """
+        raise NotImplementedError()
+
+    def commandedTorque(self):
+        """Retrieves the currently commanded joint torque. 
+        """
+        raise NotImplementedError()
+
+    def destinationPosition(self):
+        """Retrieves the destination of a motion queue controller.
+        """
+        raise NotImplementedError()
+
+    def destinationVelocity(self):
+        """Retrieves the final velocity of a motion queue controller.
+        """
+        raise NotImplementedError()
+    
+    def destinationTime(self):
+        """Retrieves the final clock time of a motion queue controller.
+        """
+        raise NotImplementedError()
+
+    def queuedTrajectory(self):
+        """Returns a trajectory starting from the current time representing all
+        commands in a motion queue controller.
+
+        Returns:
+            tuple: either (ts,qs) or (ts,qs,vs) representing a piecewise linear
+            or a piecewise-cubic trajectory.
+        """
+        raise NotImplementedError()
+
+    def cartesianPosition(self,q,frame='world'):
+        """Converts from a joint position vector to a cartesian position.
+
+        Args:
+            q (vector): the robot's joint positions.
+            frame (str): either 'world', 'base', 'end effector', or 'tool'.
+                Note: 'end effector' and 'tool' don't make any sense here,
+                since the tool frame is constant relative to these frames...
+
+        Returns:
+            Klampt se3 element: specifies end effector Cartesian transform
+            relative to the given frame.
+        """
+        raise NotImplementedError()
+
+    def cartesianVelocity(self,q,dq,frame='world'):
+        """Converts from a joint position / velocity vector to a cartesian
+        velocity.
+
+        Args: 
+            q (vector): the robot's joint positions.
+            dq (vector): the robot's joint velocities.
+            frame (str): either 'world', 'base', 'end effector', or 'tool'.
+                Note: if 'base' is specified, the velocity of the base is 
+                subtracted from the reported speed.
+
+        Returns:
+            (w,v): specifies end effector Cartesian angular velocity/velocity
+            relative to the given frame.
+        """
+        raise NotImplementedError()
+
+    def cartesianForce(self,q,t,frame='world'):
+        """Converts from a joint position / torque vector to a cartesian force.
+
+        Args: 
+            q (vector): the robot's joint positions.
+            t (vector): the robot's joint torques.
+            frame (str): either 'world', 'base', 'end effector', or 'tool'.
+
+        Returns:
+            (t,f): specifies end effector Cartesian torque/force relative to
+            given frame.
+        """
+        raise NotImplementedError()
+
+    def sensedCartesianPosition(self,frame='world'):
+        return self.cartesianPosition(self.sensedPosition(),frame)
+
+    def sensedCartesianVelocity(self,frame='world'):
+        return self.cartesianVelocity(self.sensedPosition(),self.sensedVelocity(),frame)
+
+    def sensedCartesianForce(self,frame='world'):
+        return self.cartesianForce(self.sensedPosition(),self.sensedTorque(),frame)
+
+    def commandedCartesianPosition(self,frame='world'):
+        return self.cartesianPosition(self.commandedPosition(),frame)
+
+    def commandedCartesianVelocity(self,frame='world'):
+        return self.cartesianVelocity(self.commandedPosition(),self.commandedVelocity(),frame)
+
+    def commandedCartesianForce(self,frame='world'):
+        return self.cartesianForce(self.commandedPosition(),self.commandedTorque(),frame='world')
+
+    def destinationCartesianPosition(self,frame='world'):
+        """Retrieves the Cartesian destination of a motion queue controller.
+
+        Args:
+            frame (str): either 'world', 'base', 'end effector', or 'tool'
+        """
+        return self.cartesianPosition(self.destinationPosition(),frame)
+
+    def destinationCartesianVelocity(self,frame='world'):
+        """Retrieves the final Cartesian velocity of a motion queue controller.
+
+        Args:
+            frame (str): either 'world', 'base', 'end effector', or 'tool'
+        """
+        return self.cartesianVelocity(self.destinationPosition(),self.desinationVelocity())
+
+    def queuedCartesianTrajectory(self,frame='world'):
+        """Returns the Cartesian trajectory starting from the current time 
+        representing all commands in a motion queue controller.
+
+        Args:
+            frame (str): either 'world', 'base', 'end effector', or 'tool'
+
+        Returns:
+            tuple: either (ts,Ts) or (ts,Ts,dTs) representing a piecewise
+            linear or a piecewise-cubic trajectory.  Ts is a list of Klampt se3
+            elements.  dTs is a list of (angular velocity,velocity) pairs.
+        """
+        res = self.queuedTrajectory()
+        if len(res) == 2:
+            ts,qs = res
+            return ts,[self.cartesianPosition(q,frame) for q in qs]
+        elif len(res) == 3:
+            ts,qs,vs = res
+            return ts,[self.cartesianPosition(q,frame) for q in qs],[self.cartesianVelocity(q,dq,frame) for q,dq in zip(qs,vs)]
+        else:
+            raise RuntimeError("Invalid result from queuedTrajectory")
+
     def setPosition(self,q):
         """Sets an instantaneous position command.
 
@@ -324,171 +516,56 @@ class RobotInterfaceBase(object):
         """
         raise NotImplementedError()
 
-    def setCartesianPosition(self,xparams):
-        """Sets a Cartesian position command.  Like setPosition, this command
-        is sent in immediate mode.
+    def setCartesianPosition(self,xparams,frame='world'):
+        """Sets a Cartesian position command.  The tool is commanded to reach
+        the given coordinates relative to the given frame.  Like setPosition,
+        this command is sent in immediate mode.
 
         Args:
-            xparams: typically a klampt.math.se3 object for position /
-                orientation commands, or a 3-vector for position-only.
+            xparams: a klampt.math.se3 object for position / orientation
+            commands, or a 3-vector for position-only.
+            frame (str): either 'world', 'base', 'end effector', or 'tool'
         """
         raise NotImplementedError()
 
-    def moveToCartesianPosition(self,xparams,speed=1.0):
-        """Sets a Cartesian move-to-position command.
+    def moveToCartesianPosition(self,xparams,speed=1.0,frame='world'):
+        """Sets a Cartesian move-to-position command. The tool is commanded to reach
+        the given coordinates relative to the given frame. 
 
         Args:
             xparams: typically a klampt.math.se3 object for position /
                 orientation commands, or a 3-vector for position-only.
             speed (float, optional): The speed at which the position
                 should be reached.
+            frame (str): either 'world', 'base', 'end effector', or 'tool'
         """
         raise NotImplementedError()
 
-    def setCartesianVelocity(self,dxparams,ttl=None):
-        """Sets a Cartesian velocity command. 
+    def setCartesianVelocity(self,dxparams,ttl=None,frame='world'):
+        """Sets a Cartesian velocity command. The tool is commanded to move
+        along the given velocity(s) relative to the given frame.  
 
         Args:
             dxparams: typically an (angular velocity, translational velocity)
-                pair for position / orientation commands.  A 3-vector for
-                position-only.
+                pair for translation / orientation commands.  A 3-vector for
+                translation-only.
             ttl (float, optional): A time-to-live for this command.
+            frame (str): either 'world', 'base', 'end effector', or 'tool'
         """
         raise NotImplementedError()
 
-    def setCartesianForce(self,fparams,ttl=None):
-        """Sets a Cartesian torque command. 
+    def setCartesianForce(self,fparams,ttl=None,frame='world'):
+        """Sets a Cartesian torque command. The tool is commanded to exert
+        the given force or (torque, force) relative to the given frame.  
 
         Args:
-            fparams: typically an (torque, force) pair for position
-                / orientation commands.  A 3-vector for position-only.
+            fparams: typically an (torque, force) pair for translation
+                / orientation commands.  A 3-vector for translation-only.
             ttl (float, optional): A time-to-live for this command.
+            frame (str): either 'world', 'base', 'end effector', or 'tool'
         """
         raise NotImplementedError()
 
-    def status(self,part=None,joint_idx=None):
-        """Returns a status string for the given part / joint.  'ok' means
-        everything is OK."""
-        if part is not None or joint_idx is not None:
-            return self.getPartController(part,joint_idx).status()
-        return 'ok'
-
-    def isMoving(self,part=None,joint_idx=None):
-        """Returns true if the part / joint are currently moving"""
-        if part is not None or joint_idx is not None:
-            return self.getPartController(part,joint_idx).isMoving()
-        raise NotImplementedError()
-
-    def sensedPosition(self):
-        """Retrieves the currently sensed joint position. 
-        """
-        raise NotImplementedError()
-
-    def sensedVelocity(self):
-        """Retrieves the currently sensed joint velocity. 
-        """
-        raise NotImplementedError()
-
-    def sensedTorque(self):
-        """Retrieves the currently sensed joint torque. 
-        """
-        raise NotImplementedError()
-
-    def commandedPosition(self):
-        """Retrieves the currently commanded joint position. 
-        """
-        raise NotImplementedError()
-
-    def commandedVelocity(self):
-        """Retrieves the currently commanded joint velocity. 
-        """
-        raise NotImplementedError()
-
-    def commandedTorque(self):
-        """Retrieves the currently commanded joint torque. 
-        """
-        raise NotImplementedError()
-
-    def destinationPosition(self):
-        """Retrieves the destination of a motion queue controller.
-        """
-        raise NotImplementedError()
-
-    def destinationVelocity(self):
-        """Retrieves the final velocity of a motion queue controller.
-        """
-        raise NotImplementedError()
-    
-    def destinationTime(self):
-        """Retrieves the final clock time of a motion queue controller.
-        """
-        raise NotImplementedError()
-
-    def queuedTrajectory(self):
-        """Returns a trajectory starting from the current time representing all
-        commands in a motion queue controller.
-
-        Returns:
-            tuple: either (ts,qs) or (ts,qs,vs) representing a piecewise linear
-            or a piecewise-cubic trajectory.
-        """
-        raise NotImplementedError()
-
-    def cartesianPosition(self,q):
-        """Converts from a joint position vector to a cartesian position"""
-        raise NotImplementedError()
-
-    def cartesianVelocity(self,q,dq):
-        """Converts from a joint position / velocity vector to a cartesian velocity"""
-        raise NotImplementedError()
-
-    def cartesianForce(self,q,t):
-        """Converts from a joint position / torque vector to a cartesian force"""
-        raise NotImplementedError()
-
-    def sensedCartesianPosition(self):
-        return self.cartesianPosition(self.sensedPosition())
-
-    def sensedCartesianVelocity(self):
-        return self.cartesianVelocity(self.sensedPosition(),self.sensedVelocity())
-
-    def sensedCartesianForce(self):
-        return self.cartesianForce(self.sensedPosition(),self.sensedTorque())
-
-    def commandedCartesianPosition(self):
-        return self.cartesianPosition(self.commandedPosition())
-
-    def commandedCartesianVelocity(self):
-        return self.cartesianVelocity(self.commandedPosition(),self.commandedVelocity())
-
-    def commandedCartesianForce(self):
-        return self.cartesianForce(self.commandedPosition(),self.commandedTorque())
-
-    def destinationCartesianPosition(self):
-        """Retrieves the Cartesian destination of a motion queue controller"""
-        return self.cartesianPosition(self.destinationPosition())
-
-    def destinationCartesianVelocity(self):
-        """Retrieves the final Cartesian velocity of a motion queue controller"""
-        return self.cartesianVelocity(self.destinationPosition(),self.desinationVelocity())
-
-    def queuedCartesianTrajectory(self):
-        """Returns the Cartesian trajectory starting from the current time 
-        representing all commands in a motion queue controller.
-
-        Returns:
-            tuple: either (ts,Ts) or (ts,Ts,dTs) representing a piecewise linear
-            or a piecewise-cubic trajectory.
-        """
-        res = self.queuedTrajectory()
-        if len(res) == 2:
-            ts,qs = res
-            return ts,[self.cartesianPosition(q) for q in qs]
-        elif len(res) == 3:
-            ts,qs,vs = res
-            return ts,[self.cartesianPosition(q) for q in qs],[self.cartesianVelocity(q,dq) for q,dq in zip(qs,vs)]
-        else:
-            raise RuntimeError("Invalid result from queuedTrajectory")
 
     def partToRobotConfig(self,pconfig,part,robotConfig,joint_idx=None):
         """Fills a configuration vector for the whole robot given the
