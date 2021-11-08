4565dd5bec93db9e303cebfa44f9c2571908449b
Kris Hauser <kkhauser@illinois.edu>
2021-11-08 00:54:54 -0500

    Added typing hints and convenience functions for converting to/from numpy

--- Python/klampt/math/se3.py
+++ Python/klampt/math/se3.py
@@ -23,12 +23,14 @@ and ``klampt.io.loader.read_se3()``.
 
 from . import vectorops
 from . import so3
+from typing import List,Tuple,Callable
+from ..model.typing import Vector3,Rotation,RigidTransform
 
-def identity():
+def identity() -> RigidTransform:
     """Returns the identity transformation."""
     return ([1.,0.,0.,0.,1.,0.,0.,0.,1.],[0.,0.,0.])
 
-def inv(T):
+def inv(T : RigidTransform) -> RigidTransform:
     """Returns the inverse of the transformation."""
     (R,t) = T
     Rinv = so3.inv(R)
@@ -37,48 +39,59 @@ def inv(T):
             -Rinv[2]*t[0]-Rinv[5]*t[1]-Rinv[8]*t[2]]
     return (Rinv,tinv)
 
-def apply(T,point):
+def apply(T : RigidTransform, point: Vector3) -> Vector3:
     """Applies the transform T to the given point"""
     return vectorops.add(apply_rotation(T,point),T[1])
 
-def apply_rotation(T,point):
+def apply_rotation(T : RigidTransform, point : Vector3) -> Vector3:
     """Applies only the rotation part of T"""
     R = T[0]
     return so3.apply(R,point)
 
-def rotation(T):
+def rotation(T : RigidTransform) -> Rotation:
     """Returns the 3x3 rotation matrix corresponding to T's rotation"""
     (R,t) = T
     return so3.matrix(R)
 
-def from_rotation(mat):
+def from_rotation(mat : List[List[float]]) -> RigidTransform:
     """Returns a transformation T corresponding to the 3x3 rotation matrix mat"""
     R = so3.from_matrix(mat)
     return (R,[0.,0.,0.])
 
-def translation(T):
+def translation(T : RigidTransform) -> Vector3:
     """Returns the translation vector corresponding to T's translation"""
     return T[1]
 
-def from_translation(t):
+def from_translation(t : Vector3) -> RigidTransform:
     """Returns a transformation T that translates points by t"""
     return (so3.identity(),t[:])
 
-def homogeneous(T):
-    """Returns the 4x4 homogeneous transform corresponding to T"""
+def homogeneous(T : RigidTransform) -> List[List[float]]:
+    """Returns the 4x4 homogeneous transform corresponding to T."""
     (R,t) = T
     return [[R[0],R[3],R[6],t[0]],
             [R[1],R[4],R[7],t[1]],
             [R[2],R[5],R[8],t[2]],
             [0.,0.,0.,1.]]
 
-def from_homogeneous(mat):
-    """Returns a T corresponding to the 4x4 homogeneous transform mat"""
+def from_homogeneous(mat : List[List[float]]) -> RigidTransform:
+    """Returns an se3 transform corresponding to the 4x4 homogeneous
+    transform matrix mat."""
     t = [mat[0][3],mat[1][3],mat[2][3]]
     R = [mat[0][0],mat[1][0],mat[2][0],mat[0][1],mat[1][1],mat[2][1],mat[0][2],mat[1][2],mat[2][2]]
     return (R,t)
 
-def mul(T1,T2):
+def ndarray(T : RigidTransform) -> "ndarray":
+    """Returns the 4x4 homogeneous transform corresponding to T."""
+    import numpy
+    return numpy.array(homogeneous(T))
+
+def from_ndarray(mat : "ndarray") -> "RigidTransform":
+    """Returns an se3 transform from a 4x4 Numpy array representing the
+    homogeneous transform."""
+    return from_homogeneous(mat)
+
+def mul(T1 : RigidTransform, T2 : RigidTransform) -> RigidTransform:
     """Composes two transformations."""
     (R1,t1) = T1
     (R2,t2) = T2
@@ -86,7 +99,7 @@ def mul(T1,T2):
     t = vectorops.add(so3.apply(R1,t2),t1)
     return (R,t)
 
-def distance(T1,T2,Rweight=1.0,tweight=1.0):
+def distance(T1 : RigidTransform, T2 : RigidTransform, Rweight=1.0,tweight=1.0) -> float:
     """Returns a distance metric between the two transformations. The
     rotation distance is weighted by Rweight and the translation distance
     is weighted by tweight"""
@@ -94,7 +107,7 @@ def distance(T1,T2,Rweight=1.0,tweight=1.0):
     (R2,t2)=T2
     return Rweight*so3.distance(R1,R2) + tweight*vectorops.distance(t1,t2)
 
-def error(T1,T2):
+def error(T1 : RigidTransform, T2 : RigidTransform) -> float:
     """Returns a 6D "difference vector" that describes how far T1 is from T2.
     More precisely, this is the Lie derivative (w,v)."""
     (R1,t1)=T1
@@ -102,11 +115,11 @@ def error(T1,T2):
     #concatenate lists
     return so3.error(R1,R2) + vectorops.sub(t1,t2)
 
-def interpolate(T1,T2,u):
+def interpolate(T1 : RigidTransform, T2 : RigidTransform, u : float) -> RigidTransform:
     """Interpolate linearly between the two transformations T1 and T2."""
     return (so3.interpolate(T1[0],T2[0],u),vectorops.interpolate(T1[1],T2[1],u))
 
-def interpolator(T1,T2):
+def interpolator(T1 : RigidTransform, T2 : RigidTransform) -> Callable:
     """Returns a function of one parameter u that interpolates linearly
     between the two transformations T1 and T2. After f(u) is constructed, calling
     f(u) is about 2x faster than calling interpolate(T1,T2,u)."""
