8d5c36f91490443747da9263a01c5f1eabd6833b
Kris Hauser <kkhauser@illinois.edu>
2022-02-10 16:02:53 -0500

    Now making tinyxml as universal

--- Python/make_deps_macos.sh
+++ Python/make_deps_macos.sh
@@ -1,11 +1,21 @@
-set -e
-brew install assimp glew
+set -e  #error if any step fails
+
+brew install glew
 brew uninstall --ignore-dependencies pkg-config
 pip install cmake
 
 pushd ../Cpp/Dependencies
 make unpack-deps
-make dep-tinyxml && make dep-ode
+git clone https://github.com/assimp/assimp.git
+pushd assimp
+cmake . -DCMAKE_OSX_ARCHITECTURES="arm64;x86_64"
+make -j 
+make install
+popd
+pushd tinyxml
+make ARCHS="-arch x86_64 -arch arm64"
+popd
+make dep-ode
 pushd KrisLibrary
 git checkout devel
 cmake . -DC11_ENABLED=ON -DUSE_GLUT=OFF -DUSE_GLUI=OFF -DCMAKE_OSX_ARCHITECTURES="arm64;x86_64"
