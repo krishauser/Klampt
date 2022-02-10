39cbb057c766bdc6bca000eb9bfa5d5a59581385
Kris Hauser <kkhauser@illinois.edu>
2022-02-10 15:24:10 -0500

    Made checker parse the setup.py to get the list of libs

--- Python/check_mac_universal.py
+++ Python/check_mac_universal.py
@@ -1,8 +1,18 @@
-import setup
-
 import subprocess
+
+libline = None
+with open('setup.py','r') as f:
+    for line in f.readlines():
+        if line.startswith('libs = '):
+            libline = line
+            break
+
+libs = libline.split('"""')[1].split(';')
+print(libs)
+
+
 #print results of lipo
-for l in setup.full_libs:
+for l in libs:
     if not l.endswith('.platform'):
         try:
             print(subprocess.check_output(["lipo","-info",l]).decode('utf-8').strip())
