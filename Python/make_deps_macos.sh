f2786eb20536462088cfc7f51e03415c2b830550
Kris Hauser <kkhauser@illinois.edu>
2022-02-10 15:28:49 -0500

    Fixed build deps script

--- Python/make_deps_macos.sh
+++ Python/make_deps_macos.sh
@@ -1,3 +1,4 @@
+set -e
 brew install assimp glew
 brew uninstall --ignore-dependencies pkg-config
 pip install cmake
@@ -7,10 +8,12 @@ make unpack-deps
 make dep-tinyxml && make dep-ode
 pushd KrisLibrary
 git checkout devel
-cmake . -DC11_ENABLED=ON -DUSE_GLUT=OFF -DUSE_GLUI=OFF -DCMAKE_OSX_ARCHITECTURES=\"arm64;x86_64\" && make -j
+cmake . -DC11_ENABLED=ON -DUSE_GLUT=OFF -DUSE_GLUI=OFF -DCMAKE_OSX_ARCHITECTURES="arm64;x86_64"
+make -j
 popd
 popd
 
 pushd ..
-cmake . -DUSE_GLUT=OFF -DUSE_GLUI=OFF -DCMAKE_OSX_ARCHITECTURES=\"arm64;x86_64\" && make -j Klampt
+cmake . -DUSE_GLUT=OFF -DUSE_GLUI=OFF -DCMAKE_OSX_ARCHITECTURES="arm64;x86_64"
+make -j Klampt
 popd
