acda21a5ec0eb49fa7bb1f24f9fdc4b01fa764c9
Kris Hauser <kkhauser@illinois.edu>
2023-11-28 21:35:23 -0500

    Only auditwheel for Klampt wheels

--- dist/linux_Python/update-klampt-build-wheels.sh
+++ dist/linux_Python/update-klampt-build-wheels.sh
@@ -6,18 +6,18 @@ cd /Klampt; git pull; git checkout master; git pull; make -j 8 Klampt
 cp /Klampt/Python/setup.py /io/wheelhouse
 
 # Compile wheels
-for PYBIN in /opt/python/cp*/bin; do
+for PYBIN in /opt/python/p*/bin; do
     echo "$PYBIN"
     "${PYBIN}/pip" wheel /Klampt/Python/ -w /wheelhouse/
 done
 
 # Bundle external shared libraries into the wheels
-for whl in /wheelhouse/*.whl; do
+for whl in /wheelhouse/Klampt*.whl; do
     auditwheel repair --exclude libGLX.so.0 --exclude libGLdispatch.so.0 "$whl" -w /io/wheelhouse/
 done
 
 # Install packages and test
-for PYBIN in /opt/python/cp*/bin/; do
+for PYBIN in /opt/python/p*/bin/; do
     "${PYBIN}/pip" install Klampt --no-index -f /io/wheelhouse
     #(cd "$HOME"; "${PYBIN}/nosetests" pymanylinuxdemo)
 done
