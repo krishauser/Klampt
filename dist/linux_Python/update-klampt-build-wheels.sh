b0cebbf3f24e4c6f1066dcadd92a289eb413a750
Kris Hauser <kkhauser@illinois.edu>
2023-11-28 21:36:38 -0500

    PyPy build now works

--- dist/linux_Python/update-klampt-build-wheels.sh
+++ dist/linux_Python/update-klampt-build-wheels.sh
@@ -6,7 +6,7 @@ cd /Klampt; git pull; git checkout master; git pull; make -j 8 Klampt
 cp /Klampt/Python/setup.py /io/wheelhouse
 
 # Compile wheels
-for PYBIN in /opt/python/p*/bin; do
+for PYBIN in /opt/python/*/bin; do
     echo "$PYBIN"
     "${PYBIN}/pip" wheel /Klampt/Python/ -w /wheelhouse/
 done
@@ -17,7 +17,7 @@ for whl in /wheelhouse/Klampt*.whl; do
 done
 
 # Install packages and test
-for PYBIN in /opt/python/p*/bin/; do
+for PYBIN in /opt/python/*/bin/; do
     "${PYBIN}/pip" install Klampt --no-index -f /io/wheelhouse
     #(cd "$HOME"; "${PYBIN}/nosetests" pymanylinuxdemo)
 done
