b26219e730cface10e575d2afb3b62b720cd0aa3
krishauser <hauser.kris@gmail.com>
2023-12-01 18:01:35 -0500

    Added separate triggers for macos and manylinux due to debugging

---
+++ Python/make_deps_linux.sh
@@ -0,0 +1,34 @@
+set -e  #error if any step fails
+
+yum clean all
+yum install -y glpk glpk-devel mesa-libGLU-devel git
+pip install cmake
+
+git clone https://github.com/krishauser/Klampt.git
+
+pushd Klampt/Cpp/Dependencies
+make unpack-deps
+git clone https://github.com/assimp/assimp.git
+
+pushd assimp
+cmake -DASSIMP_BUILD_TESTS=OFF . && make -j 8 && make install
+popd
+
+pushd glew-2.1.0
+make -j && make install
+popd
+
+make dep-tinyxml && make dep-ode
+
+pushd KrisLibrary
+cmake . -DC11_ENABLED=ON -DUSE_GLUT=OFF -DUSE_GLUI=OFF  && make -j
+popd
+
+popd
+
+pushd Klampt
+cmake . && make -j Klampt
+make -j Pack
+bin/Pack
+popd
+
