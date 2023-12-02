8d221e5ccfc84c39c0ad36798298aa98b6e9ee79
Kris Hauser <kkhauser@illinois.edu>
2023-12-01 22:10:03 -0500

    Updated internal build linux instructions to get versions up to date

--- dist/linux_Python/build-docker-containers.sh
+++ dist/linux_Python/build-docker-containers.sh
@@ -11,4 +11,4 @@ docker pull quay.io/pypa/manylinux2014_x86_64
 #docker pull quay.io/pypa/manylinux_2_28_aarch64
 docker build --no-cache -t manylinux_klampt_x86_64 -f Dockerfile_x86_64 .
 #docker build --no-cache -t manylinux_klampt_i686 -f Dockerfile_i686 .
-#docker build --no-cache -t manylinux_klampt_aarch64 -f Dockerfile_aarch64 .
+#docker build --no-cache --platform=linux/arm64 -t manylinux_klampt_aarch64 -f Dockerfile_aarch64 .
--- dist/linux_Python/update-klampt-build-wheels.sh
+++ dist/linux_Python/update-klampt-build-wheels.sh
@@ -2,8 +2,7 @@
 set -e
 
 cd /Klampt/Cpp/Dependencies/KrisLibrary; git checkout master; git pull; make -j 8
-cd /Klampt; git pull; git checkout master; git pull; make -j 8 Klampt
-cp /Klampt/Python/setup.py /io/wheelhouse
+cd /Klampt; git pull; git checkout master; git pull; cmake .; make -j 8 Klampt
 
 # Compile wheels
 for PYBIN in /opt/python/*/bin; do
@@ -13,11 +12,13 @@ done
 
 # Bundle external shared libraries into the wheels
 for whl in /wheelhouse/Klampt*.whl; do
-    auditwheel repair --exclude libGLX.so.0 --exclude libGLdispatch.so.0 "$whl" -w /io/wheelhouse/
+    auditwheel repair --exclude libGLdispatch.so.0 --exclude libGLX.so.0 "$whl" -w /io/wheelhouse/
 done
 
 # Install packages and test
 for PYBIN in /opt/python/*/bin/; do
+    "${PYBIN}/pip" install PyOpenGL
+    "${PYBIN}/pip" install numpy
     "${PYBIN}/pip" install Klampt --no-index -f /io/wheelhouse
     #(cd "$HOME"; "${PYBIN}/nosetests" pymanylinuxdemo)
 done
