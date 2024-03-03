72047f53ceb60a52086cd64dc12b0bd67c2b3b40
Kris Hauser <hauser.kris@gmail.com>
2024-03-03 11:04:30 -0500

    Different build path for GLPK on Aarch64

---
+++ Python/make_deps_linux_aarch64.sh
@@ -0,0 +1,51 @@
+#assumed to run in a Docker container containing Klampt directory
+##assumed to run in a Docker container only containing Python directory
+
+set -e  #error if any step fails
+
+yum clean all
+yum install -y mesa-libGLU-devel git
+pip install cmake
+
+#no glpk available on aarch64
+git clone https://github.com/firedrakeproject/glpk.git
+pushd glpk
+./configure --disable-shared
+make CFLAGS=-O3
+make install
+popd
+
+#git clone https://github.com/krishauser/Klampt.git
+
+#pushd Klampt/Cpp/Dependencies
+pushd Cpp/Dependencies
+make unpack-deps
+git clone https://github.com/assimp/assimp.git
+
+pushd assimp
+cmake -DASSIMP_BUILD_TESTS=OFF . && make -j && make install
+popd
+
+pushd glew-2.1.0
+make -j && make install
+popd
+
+make dep-tinyxml && make dep-ode
+
+pushd KrisLibrary
+cmake . -DC11_ENABLED=ON -DUSE_GLUT=OFF -DUSE_GLUI=OFF  && make -j
+popd
+
+popd
+
+#pushd Klampt
+cmake . && make -j Klampt
+make -j Pack
+bin/Pack
+#popd
+
+#echo "Working directory for cmake . call:"
+#pwd
+
+#update Python CMakeList from git pull'ed Klampt
+#cmake -DKLAMPT_ROOT=Klampt -DKLAMPT_DEPENDENCIES=Klampt/Cpp/Dependencies Python
