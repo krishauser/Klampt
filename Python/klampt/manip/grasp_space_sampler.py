9b6e373e62726ef20f988a6c49acbc2dcd12c35f
Kris Hauser <hauser.kris@gmail.com>
2025-08-19 15:55:39 -0400

    Added ability to sample near object surface

--- Python/klampt/manip/grasp_space_sampler.py
+++ Python/klampt/manip/grasp_space_sampler.py
@@ -1,7 +1,9 @@
 from .grasp_space import GraspParameterSpace
 from .grasp_sampler import GraspSamplerBase
-from klampt.model.gripperinfo import GripperInfo
-from klampt.model.typing import Config
+from ..model.gripperinfo import GripperInfo
+from ..model.typing import Config
+from ..math import vectorops,so3,se3
+from .. import Geometry3D, RigidObjectModel
 from .grasp import Grasp
 import random
 from typing import Optional,Union,List,Callable
@@ -10,16 +12,23 @@ class GraspSpaceSampler(GraspSamplerBase):
     """A grasp sampler that samples grasps uniformly from a GraspParameterSpace
     and filters and ranks them using a user-provided function.
 
+    The scoring function should return a value in [0,1] for each grasp, where
+    0 is the worst and 1 is the best. 
+
     If num_initial_samples > 0, then that many samples will be drawn from the
     space, scored, and filtered by the threshold function.  The remaining
     samples will be sorted by score, and returned in that order.  If
     num_initial_samples=0, then samples will be drawn from the space on-the-fly
     and scored, and no filtering will be done.
+
+    If distance_to_object is given, then the sampled grasps will be sampled
+    with the contact point a range of at most that distance to the object.
     """
     def __init__(self,
                  space : GraspParameterSpace,
                  score_function : Callable[[Grasp], float],
                  threshold : Union[Callable[[Grasp], bool],float] = 0.0,
+                 distance_to_object : Optional[float] = None,
                  num_initial_samples : int = 100,
                  num_retries : int = 0,
                  object_relative : bool = False):
@@ -30,6 +39,7 @@ class GraspSpaceSampler(GraspSamplerBase):
             self.threshold = lambda s: s.score >= threshold
         else:
             self.threshold = threshold
+        self.distance_to_object = distance_to_object
         self.num_initial_samples = num_initial_samples
         self.num_retries = num_retries
         self.samples = []  # type: List[Grasp]
@@ -48,6 +58,16 @@ class GraspSpaceSampler(GraspSamplerBase):
             self.transform = None
         if self.num_initial_samples == 0:
             return True #generate on the fly, no filtering
+        if self.distance_to_object is not None:
+            from ..model.geometry import sample_surface
+            g = object if isinstance(object,Geometry3D) else object.geometry()
+            object_points = sample_surface(g, self.num_initial_samples, local=False)
+            #fuzz by distance_to_object
+            if self.distance_to_object > 0:
+                for i,p in enumerate(object_points):
+                    e = (random.gauss(0,1),random.gauss(0,1),random.gauss(0,1))
+                    object_points[i] = vectorops.madd(object_points[i],vectorops.unit(e),self.distance_to_object*random.uniform(0,1)**(1.0/3.0))
+            self.object_points = object_points
         self.samples = []
         for _ in range(self.num_initial_samples):
             g = self.sampleGraspSpace()
@@ -88,7 +108,14 @@ class GraspSpaceSampler(GraspSamplerBase):
         """Returns a scored grasp, sampled uniformly at random from the grasp space."""
         f = [random.uniform(b[0], b[1]) for b in zip(*self.featureBounds)]
         g = self.space.toGrasp(f)
+
         if self.transform is not None:
             g = g.getTransformed(self.transform)
+
+        if self.distance_to_object is not None:
+            #move the contact centroid to a random point sampled near the object surface
+            old_center = se3.apply(g.asFixedGrasp().ikConstraint.getTransform(),self.space.gripper.estimatedContactCentroid())
+            g = g.getTransformed((so3.identity(),vectorops.sub(random.choice(self.object_points),old_center)))
+
         g.score = self.score_function(g)
         return g
